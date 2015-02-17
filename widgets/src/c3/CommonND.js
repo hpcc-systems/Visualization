e5f3d96cdb1ee49fcd957db2e696a2f56977ddc9
Gordon Smith <gordon.smith@lexisnexis.com>
2015-02-17 17:26:50 +0000

    Upgrade to C3 Latest. Add support for subchart Add support for different x-axis types. Common up similar code.
    Signed-off-by: Gordon Smith <gordon.smith@lexisnexis.com>

--- widgets/src/c3/CommonND.js
+++ widgets/src/c3/CommonND.js
@@ -22,17 +22,81 @@
         this._config.data.color = function (color, d) {
             return context._palette(d.id ? d.id : d);
         };
+        this._prevColumns = [];
     };
     CommonND.prototype = Object.create(Common.prototype);
     CommonND.prototype.implements(INDChart.prototype);
 
     CommonND.prototype.publish("paletteID", "default", "set", "Palette ID", CommonND.prototype._palette.switch());
+    CommonND.prototype.publish("xaxis_type", "category", "set", "X-Axis Type", ["category", "timeseries", "indexed"]);
+    CommonND.prototype.publish("subchart", false, "boolean", "Show SubChart");
+
+    CommonND.prototype.getDiffC3Columns = function () {
+        return this._prevColumns.filter(function (i) { return this._columns.indexOf(i) < 0; }, this);
+    };
+
+    CommonND.prototype.render = function () {
+        var retVal = Common.prototype.render.apply(this, arguments);
+        this._prevColumns = this._columns;
+        return retVal;
+    };
+
+    CommonND.prototype.enter = function (domNode, element) {
+        if (this._subchart) {
+            this._config.subchart = {
+                show: true, size: {
+                    height: 20
+                }
+            };
+        }
+
+        this._config.axis.x = {
+            type: this._xaxis_type
+        };
+
+        switch (this._xaxis_type) {
+        case "category":
+            this._config.axis.tick = {
+                centered: true,
+                multiline: false
+            }
+            break;
+        case "timeseries":
+            this._config.data.x = this._columns[0];
+            this._config.axis.tick = {
+                format: '%d %b %Y'
+            }
+            break;
+        }
+
+        Common.prototype.enter.apply(this, arguments);
+    };
 
     CommonND.prototype.update = function (domNode, element) {
         Common.prototype.update.apply(this, arguments);
         this._palette = this._palette.switch(this._paletteID);
     };
 
+    CommonND.prototype.update = function (domNode, element) {
+        Common.prototype.update.apply(this, arguments);
+
+        switch (this._xaxis_type) {
+            case "category":
+                this.c3Chart.load({
+                    categories: this.getC3Categories(),
+                    columns: this.getC3Columns(),
+                    unload: this.getDiffC3Columns()
+                });
+                break;
+            case "indexed":
+            case "timeseries":
+                this.c3Chart.load({
+                    columns: this.getC3Columns(),
+                    unload: this.getDiffC3Columns()
+                });
+                break;
+        }
+    };
 
     return CommonND;
 }));

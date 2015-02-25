e2b8d15e1e2bda140c3f186d542012ffdade571b
Mathew Zummo <mzummo@gmail.com>
2015-02-25 14:35:26 -0500

    Adds Pagination widget and modifies Table widget
    Signed-off-by: Mathew Zummo <mzummo@gmail.com>

--- src/other/Table.js
+++ src/other/Table.js
@@ -1,11 +1,11 @@
 "use strict";
 (function (root, factory) {
     if (typeof define === "function" && define.amd) {
-        define(["../common/HTMLWidget", "css!./Table"], factory);
+        define(["../common/HTMLWidget","../other/Paginator", "css!./Table"], factory);
     } else {
         root.other_Table = factory(root.common_HTMLWidget);
     }
-}(this, function (HTMLWidget) {
+}(this, function (HTMLWidget, Paginator) {
     function Table() {
         HTMLWidget.call(this);
         this._class = "other_Table";
@@ -13,26 +13,15 @@
         this._tag = "table";
 
         this._columns = [];
+
+        this._paginator = new Paginator();
     };
     Table.prototype = Object.create(HTMLWidget.prototype);
 
-    Table.prototype.testData = function () {
-        this
-            .columns(["Lat", "Long", "Pin"])
-            .data([
-                [37.665074, -122.384375, "green-dot.png"],
-                [32.690680, -117.178540],
-                [39.709455, -104.969859],
-                [41.244123, -95.961610],
-                [32.688980, -117.192040],
-                [45.786490, -108.526600],
-                [45.796180, -108.535652],
-                [45.774320, -108.494370],
-                [45.777062, -108.549835, "red-dot.png"]
-            ])
-        ;
-        return this;
-    };
+    Table.prototype.publish("pagination", true, "boolean", "enable or disable pagination");
+    Table.prototype.publishProxy("itemsPerPage", "_paginator");
+    Table.prototype.publishProxy("_numItems", "_paginator", "numItems", true);
+    Table.prototype.publishProxy("pageNumber", "_paginator", "pageNumber", true);
 
     Table.prototype.enter = function (domNode, element) {
         HTMLWidget.prototype.enter.apply(this, arguments);
@@ -57,7 +46,44 @@
             .remove()
         ;
 
-        var rows = this.tbody.selectAll("tr").data(this._data);
+        if (this.pagination()) {
+
+            if (this._paginator.target() == null) {
+
+                var pnode = document.getElementById(this.id()).parentNode.id 
+                this._paginator.target(pnode); 
+
+            }
+
+            this._numItems(this._data.length);
+            this._tNumPages = Math.ceil(this._numItems() / this.itemsPerPage()) || 1; // TODO make this into a function in paginator?
+            if (this.pageNumber() > this._tNumPages ) { this.pageNumber(1); } // resets if current pagenum selected out of range
+
+            this._paginator._onSelect = function(p, d) {
+                console.log('page: '+p);
+                // context.pageNumber(p); // technically not needed due to tight coupling of widget
+                context.render();
+                return;
+            };
+            
+        } else {
+            this._numItems(0); // remove widget
+        }
+        
+        // pageNumber starts at index 1
+        var startIndex = this.pageNumber()-1;
+        var itemsOnPage = this.itemsPerPage();
+        
+        var start = startIndex * itemsOnPage;
+        var end = parseInt(startIndex * itemsOnPage) + parseInt(itemsOnPage);
+
+        if (this.pagination()) {
+            var tData = this._data.slice(start,end);
+        } else {
+            var tData = this._data;
+        }
+
+        var rows = this.tbody.selectAll("tr").data(tData);
         rows
             .enter()
             .append("tr")
@@ -65,6 +91,7 @@
                 context.click(context.rowToObj(d));
             })
         ;
+
         rows.exit()
             .remove()
         ;
@@ -86,6 +113,8 @@
         cells.exit()
             .remove()
         ;
+
+        this._paginator.render();
     };
 
     Table.prototype.exit = function (domNode, element) {
@@ -94,6 +123,23 @@
     };
 
     Table.prototype.click = function (d) {
+    };
+
+    Table.prototype.testData = function(_) {
+        this.columns(["Lat", "Long", "Pin"])
+        this.data([
+            [ 37.665074, -122.384375, "green-dot.png" ],
+            [ 32.690680, -117.178540 ],
+            [ 39.709455, -104.969859 ],
+            [ 41.244123, -95.961610 ],
+            [ 32.688980, -117.192040 ],
+            [ 45.786490, -108.526600 ],
+            [ 45.796180, -108.535652 ],
+            [ 45.774320, -108.494370 ],
+            [ 45.777062, -108.549835, "red-dot.png" ]
+        ]);
+        
+        return this;
     };
 
     return Table;

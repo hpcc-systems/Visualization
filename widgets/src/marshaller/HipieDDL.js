cdafd1c86355f6105e06039527ae4ae31c55c469
Gordon Smith <gordon.smith@lexisnexis.com>
2015-03-18 18:28:58 +0000

    SYMP_AVE_HACK
    Signed-off-by: Gordon Smith <gordon.smith@lexisnexis.com>

--- widgets/src/marshaller/HipieDDL.js
+++ widgets/src/marshaller/HipieDDL.js
@@ -60,6 +60,17 @@
                 if (val === undefined) {
                     val = item[rhsKey.toLowerCase()];
                 }
+                //  Symposium AVE Hack 
+                if (val === undefined && rhsKey.indexOf("_AVE") === rhsKey.length - 4 && item.base_count !== undefined) {
+                    var rhsSum = rhsKey.substring(0, rhsKey.length - 4) + "_SUM";
+                    val = item[rhsSum];
+                    if (val === undefined) {
+                        val = item[rhsSum.toLowerCase()];
+                    }
+                    if (val) {
+                        val /= item.base_count;
+                    }
+                }
                 retVal[this.columnsIdx[key]] = val;
             }
         } catch (e) {

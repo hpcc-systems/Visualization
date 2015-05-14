GordonSmith/hpcc-js

Param Refactor ALL
Signed-off-by: Mathew Zummo <mzummo@gmail.com>

src/amchart/CommonXY.js
@@ -19,44 +19,93 @@
     };
 
     CommonXY.prototype = Object.create(HTMLWidget.prototype);
+    
+    /**
+     * Publish Params Common To Other Libraries
+     */
+    //TODO: need to add this globally to all widgets as well
+    CommonXY.prototype.publish("fontSize", null, "number", "Font Size",null,{tags:['Basic','TODO2']});
+    CommonXY.prototype.publish("fontFamily", null, "string", "Font Name",null,{tags:['Basic','TODO2']});
+    CommonXY.prototype.publish("fontColor", null, "html-color", "Font Color",null,{tags:['Basic','TODO2']});
+    
+    CommonXY.prototype.publish("lineWidth", 0, "number", "Line Thickness", null, {min:0,max:10,step:1,inputType:'range',tags:['Basic','TODO2']});
+    CommonXY.prototype.publish("lineColor", null, "html-color", "Color of the data/content lines",null,{tags:['Basic','TODO2']});
+    CommonXY.prototype.publish("lineOpacity", 0, "number", "Line Opacity", null, {min:0,max:1,step:0.001,inputType:'range',tags:['Basic','TODO2']}); 
+    
+    CommonXY.prototype.publish("dashedLineStyle", 0, "number", "",null,{tags:['Advanced','TODO2']});
+      
+    CommonXY.prototype.publish("yAxisTitle", "Axis title", "string", "Y-Axis Title",null,{tags:['Basic','TODO2']});
+    CommonXY.prototype.publish("xAxisTitle", "Axis title", "string", "X-Axis Title",null,{tags:['Basic','TODO2']});  
+    
+    CommonXY.prototype.publish("xAxisBaselineColor", "#000000", "html-color", "Axis color",null,{tags:['Basic','TODO2']});
+    CommonXY.prototype.publish("yAxisBaselineColor", "#000000", "html-color", "Axis color",null,{tags:['Basic','TODO2']}); // TODO ADD TO OTHER AMCHARTS
+    
+    CommonXY.prototype.publish("xAxisFontColor", null, "html-color", "Horizontal axis text style (Color)",null,{tags:['Basic']});
+    CommonXY.prototype.publish("yAxisFontColor", null, "html-color", "Vertical axis text style (Color)",null,{tags:['Basic']});
+    
+    CommonXY.prototype.publish("xAxisTitleFontSize", null, "number", "Vertical axis titletext style (Font Size)",null,{tags:['Basic','TODO2']});
+    CommonXY.prototype.publish("yAxisTitleFontSize", null, "number", "Vertical axis titletext style (Font Size)",null,{tags:['Intermediate']});
+    
+    CommonXY.prototype.publish("xAxisTitleFontColor", null, "html-color", "Color of axis value labels. Will use chart's color if not set.",null,{tags:['Basic','TODO2']});
+    CommonXY.prototype.publish("yAxisTitleFontColor", null, "html-color", "Color of axis value labels. Will use chart's color if not set.",null,{tags:['Basic','TODO2']});
 
-    CommonXY.prototype.publish("yAxisTitle", "Axis title", "string", "Y-Axis Title");
-    CommonXY.prototype.publish("xAxisTitle", "Axis title", "string", "X-Axis Title");
+    CommonXY.prototype.publish("xAxisLabelRotation", 30, "number", "X-Axis Label Rotation", null, {min:0,max:90,step:0.1,inputType:'range',tags:['Intermediate','TODO2']});
 
-    CommonXY.prototype.publish("marginLeft", 50, "number", "Margin (Left)");
-    CommonXY.prototype.publish("marginRight", 10, "number", "Margin (Right)");
-    CommonXY.prototype.publish("marginTop", 10, "number", "Margin (Top)");
-    CommonXY.prototype.publish("marginBottom", 50, "number", "Margin (Bottom)");
+    CommonXY.prototype.publish("axisLineWidth", 1, "number", "Thickness of axis",null,{tags:['Intermediate','TODO2']});
+    
+    /**
+     * Publish Params Unique To This Widget
+     */
+    CommonXY.prototype.publish("axisAlpha", 1, "number", "Axis opacity",null,{tags:['Intermediate','TODO2']}); // NOT SHARED YET BUT NEEDS TO BE
+   
+    CommonXY.prototype.publish("showScrollbar", false, "boolean", "Chart Scrollbar",null,{tags:['Intermediate','TODO2']});
+    
+    CommonXY.prototype.publish("bulletSize", 8, "number", "Bullet Size",null,{tags:['Intermediate','TODO2']});
+    CommonXY.prototype.publish("bulletType", "round", "set", "Bullet Type", ["none", "round", "square", "triangleUp", "triangleDown", "triangleLeft", "triangleRight", "bubble", "diamond"],{tags:['Intermediate','TODO2']});
+    
+    CommonXY.prototype.publish("marginLeft", 50, "number", "Margin (Left)",null,{tags:['Intermediate','TODO2']});
+    CommonXY.prototype.publish("marginRight", 10, "number", "Margin (Right)",null,{tags:['Intermediate','TODO2']});
+    CommonXY.prototype.publish("marginTop", 10, "number", "Margin (Top)",null,{tags:['Intermediate','TODO2']});
+    CommonXY.prototype.publish("marginBottom", 50, "number", "Margin (Bottom)",null,{tags:['Intermediate','TODO2']});
 
-    CommonXY.prototype.publish("chartScrollbar", false, "boolean", "Chart Scrollbar");
+    CommonXY.prototype.publish("dataDateFormat", null, "string", "",null,{tags:['Private','TODO2']});
+    
+    CommonXY.prototype.publish("xAxisAutoGridCount", true, "boolean", "Specifies whether number of gridCount is specified automatically, acoarding to the axis size",null,{tags:['Advanced','TODO2']});
+    CommonXY.prototype.publish("yAxisAutoGridCount", true, "boolean", "Specifies whether number of gridCount is specified automatically, acoarding to the axis size",null,{tags:['Advanced','TODO2']});
+    
+    CommonXY.prototype.publish("xAxisGridPosition", "middle", "set", "Specifies if a grid line is placed on the center of a cell or on the beginning of a cell", ["start","middle"],{tags:['Advanced','TODO2']});
+    CommonXY.prototype.publish("yAxisGridPosition", "middle", "set", "Specifies if a grid line is placed on the center of a cell or on the beginning of a cell", ["start","middle"],{tags:['Advanced','TODO2']});
+        
+    CommonXY.prototype.publish("xAxisFillAlpha", 0, "number", "Fill opacity. Every second space between grid lines can be filled with color. Set fillAlpha to a value greater than 0 to see the fills.",null,{tags:['Intermediate','TODO2']});
+    CommonXY.prototype.publish("yAxisFillAlpha", 0, "number", "Fill opacity. Every second space between grid lines can be filled with color. Set fillAlpha to a value greater than 0 to see the fills.",null,{tags:['Intermediate','TODO2']});
+        
+    CommonXY.prototype.publish("xAxisFillColor", "#FFFFFF", "html-color", "Fill color. Every second space between grid lines can be filled with color. Set fillAlpha to a value greater than 0 to see the fills.",null,{tags:['Intermediate','TODO2']});
+    CommonXY.prototype.publish("yAxisFillColor", "#FFFFFF", "html-color", "Fill color. Every second space between grid lines can be filled with color. Set fillAlpha to a value greater than 0 to see the fills.",null,{tags:['Intermediate','TODO2']});
 
-    CommonXY.prototype.publish("globalTooltipText","x:[[x]] y:[[y]]", "string", "Tooltip Text"); // x:[[x]] y:[[y]]
-    CommonXY.prototype.publish("graphTooltipText",["x:[[x]] y:[[y]]","x:[[x]] y:[[y]]"], "array", "Tooltip Text"); // [[category]]([[title]]): [[value]
+    CommonXY.prototype.publish("xAxisGridAlpha", 0.2, "number", "Grid alpha.",null,{tags:['Intermediate','TODO2']});
+    CommonXY.prototype.publish("yAxisGridAlpha", 0.2, "number", "Grid alpha.",null,{tags:['Intermediate','TODO2']});
 
-    CommonXY.prototype.publish("globalFillAlpha", 0, "number", "Shape Opacity", null, {min:0,max:1,step:0.001,inputType:'range'});
-    CommonXY.prototype.publish("globalLineAlpha", 0, "number", "Line Opacity", null, {min:0,max:1,step:0.001,inputType:'range'});
-    CommonXY.prototype.publish("globalLineThickness", 0, "number", "Line Thickness", null, {min:0,max:10,step:0.1,inputType:'range'})
-    CommonXY.prototype.publish("globalBulletSize", 8, "number", "Bullet Size");
-    CommonXY.prototype.publish("globalBulletType", "round", "set", "Bullet Type", ["none", "round", "square", "triangleUp", "triangleDown", "triangleLeft", "triangleRight", "bubble", "diamond"]);
-
-    CommonXY.prototype.publish("graphFillAlpha", [], "array", "Area Opacity", null, {min:0, max:1,step:0.001,inputType:'range'});
-    CommonXY.prototype.publish("graphLineAlpha", [], "array", "Area Border Opacity", null, {min:0,max:1,step:0.001,inputType:'range'});
-    CommonXY.prototype.publish("graphLineThickness", [], "array", "Line Thickness", null, {min:0,max:1,step:0.001,inputType:'range'});
-    CommonXY.prototype.publish("graphBulletSize", [], "number", "Bullet Size");
-    CommonXY.prototype.publish("graphBulletType", [], "array", "Bullet Type");
-
-    CommonXY.prototype.publish("startDuration", 0.3, "number", "Start Duration (sec)");
-
-    CommonXY.prototype.publish("dataDateFormat", null, "string", "");
-
-    CommonXY.prototype.publish("lineColor", null, "html-color", "Color of the data/content lines");
-
+    CommonXY.prototype.publish("xAxisDashLength", 0, "number", "Length of a dash. 0 means line is not dashed.",null,{tags:['Advanced','TODO2']});
+    CommonXY.prototype.publish("yAxisDashLength", 0, "number", "Length of a dash. 0 means line is not dashed.",null,{tags:['Advanced','TODO2']});
+    
+    //CommonXY.prototype.publish("yAxisMinimum", null, "number", "",null,{tags:['Intermediate','TODO2']});
+    CommonXY.prototype.publish("yAxisTitleOffset", null, "number", "",null,{tags:['Intermediate','TODO2']});
+    
+    //CommonXY.prototype.publish("startOnAxis", true, "boolean", "Draw chart starting on axis.",null,{tags:['Intermediate','TODO2']}); // DO WE NEED THIS except for override 
+    //CommonXY.prototype.publish("startDuration", 0.3, "number", "Start Duration (sec)",null,{tags:['Advanced','TODO2']}); // not coded
+    //CommonXY.prototype.publish("useImgPatterns", false, "boolean", "Enable Image Pattern backgrounds",null,{tags:['Private','TODO2']});
+    //CommonXY.prototype.publish("imgPatternArr", '["../ampatterns/black/pattern2.png"]', "string", "Background Pattern Images (Not used if '[]')",null,{inputType:'textarea',tags:['Private','TODO2']});
+    
     CommonXY.prototype.updateChartOptions = function() {
         var context = this;
 
         this._chart.theme = "none";
         this._chart.type = "xy";
         this._chart.pathToImages = "//cdn.rawgit.com/cdnjs/cdnjs/master/ajax/libs/amcharts/3.13.0/images/";
+        
+        this._chart.color = this.fontColor();
+        this._chart.fontSize = this.fontSize();
+        this._chart.fontFamily = this.fontFamily();
 
         if (this.marginLeft()) { this._chart.marginLeft = this.marginLeft(); }
         if (this.marginRight()) { this._chart.marginRight = this.marginRight(); }
@@ -66,15 +115,37 @@
         this._chart.dataDateFormat = this.dataDateFormat();
 
         this._chart.valueAxes[0].position = "bottom";
-        this._chart.valueAxes[0].axisAlpha = 0;
-        this._chart.valueAxes[0].dashLength = 1;
+        this._chart.valueAxes[0].axisAlpha = this.axisAlpha();
         this._chart.valueAxes[0].title = this.xAxisTitle();
-
+        this._chart.valueAxes[0].axisThickness = this.axisLineWidth();
+        this._chart.valueAxes[0].axisColor = this.xAxisBaselineColor();
+        this._chart.valueAxes[0].color = this.xAxisFontColor();
+        this._chart.valueAxes[0].titleFontSize = this.xAxisTitleFontSize();  
+        this._chart.valueAxes[0].titleColor = this.xAxisTitleFontColor();  
+        this._chart.valueAxes[0].labelRotation = this.xAxisLabelRotation();
+        this._chart.valueAxes[0].autoGridCount = this.xAxisAutoGridCount();
+        this._chart.valueAxes[0].gridPosition = this.xAxisGridPosition();
+        this._chart.valueAxes[0].fillAlpha = this.xAxisFillAlpha();
+        this._chart.valueAxes[0].fillColor = this.xAxisFillColor();
+        this._chart.valueAxes[0].gridAlpha = this.xAxisGridAlpha();
+        this._chart.valueAxes[0].dashLength = this.xAxisDashLength();
+        
         this._chart.valueAxes[1].position = "left";
-        this._chart.valueAxes[1].axisAlpha = 0;
-        this._chart.valueAxes[1].dashLength = 1;
+        this._chart.valueAxes[1].axisAlpha = this.axisAlpha();
         this._chart.valueAxes[1].title = this.yAxisTitle();
-
+        this._chart.valueAxes[1].axisThickness = this.axisLineWidth();
+        this._chart.valueAxes[1].axisColor = this.yAxisBaselineColor();
+        this._chart.valueAxes[1].color = this.yAxisFontColor();
+        this._chart.valueAxes[1].titleFontSize = this.yAxisTitleFontSize();  
+        this._chart.valueAxes[1].titleColor = this.yAxisTitleFontColor();  
+        this._chart.valueAxes[1].autoGridCount = this.yAxisAutoGridCount();
+        this._chart.valueAxes[1].gridPosition = this.yAxisGridPosition();
+        this._chart.valueAxes[1].fillAlpha = this.yAxisFillAlpha();
+        this._chart.valueAxes[1].fillColor = this.yAxisFillColor();
+        this._chart.valueAxes[1].gridAlpha = this.yAxisGridAlpha();
+        this._chart.valueAxes[1].dashLength = this.yAxisDashLength();
+        this._chart.valueAxes[1].axisTitleOffset = this.yAxisTitleOffset();
+        
         // DataProvider
         this._chart.dataProvider = this.formatData(this._data); 
 
@@ -85,7 +156,7 @@
         this._chart.colors = [];
             
         // Scroll Bar
-        if (this.chartScrollbar()) {
+        if (this.showScrollbar()) {
             this._chart.chartScrollbar.enabled = true;
         } else {
             this._chart.chartScrollbar.enabled = false;
@@ -98,12 +169,15 @@
         var context = this;
         var gObj = {}; 
 
-        gObj.balloonText = context.graphTooltipText()[i] || context.globalTooltipText();
-        gObj.fillAlphas = context.graphFillAlpha()[i] || context.globalFillAlpha();
-        gObj.lineAlpha = context.graphLineAlpha()[i] || context.globalLineAlpha();
-        gObj.lineThickness = context.graphLineThickness()[i] || context.globalLineThickness();
-        gObj.bullet = context.graphBulletType()[i] || context.globalBulletType();
-        gObj.bulletSize = context.graphBulletSize()[i] || context.globalBulletSize();
+        gObj.balloonText = context.tooltipText();
+        gObj.lineAlpha = context.lineOpacity();
+        gObj.lineThickness = context.lineWidth();
+        gObj.bullet = context.bulletType();
+        gObj.bulletSize = context.bulletSize();
+        gObj.dashLength = context.dashedLineStyle(); // TODO: convert to css Array Prop
+        gObj.lineAlpha = context.lineOpacity();
+        gObj.lineColor = context.lineColor();
+        gObj.lineThickness = context.lineWidth();
         
         gObj.type = gType;
 

hpcc-systems/Visualization

src/amchart/CommonXY.js
@@ -9,7 +9,7 @@
     function CommonXY() {
         HTMLWidget.call(this);
         this._tag = "div";
-        
+
         this._chart = {};
         this._data = undefined;
         this._columns = undefined;
@@ -19,43 +19,86 @@
     }
     CommonXY.prototype = Object.create(HTMLWidget.prototype);
 
-    CommonXY.prototype.publish("yAxisTitle", "Axis title", "string", "Y-Axis Title");
-    CommonXY.prototype.publish("xAxisTitle", "Axis title", "string", "X-Axis Title");
+    /**
+     * Publish Params Common To Other Libraries
+     */
 
-    CommonXY.prototype.publish("marginLeft", 50, "number", "Margin (Left)");
-    CommonXY.prototype.publish("marginRight", 10, "number", "Margin (Right)");
-    CommonXY.prototype.publish("marginTop", 10, "number", "Margin (Top)");
-    CommonXY.prototype.publish("marginBottom", 50, "number", "Margin (Bottom)");
+    CommonXY.prototype.publish("fontSize", null, "number", "Font Size",null,{tags:['Basic','Shared']});
+    CommonXY.prototype.publish("fontFamily", null, "string", "Font Name",null,{tags:['Basic','Shared']});
+    CommonXY.prototype.publish("fontColor", null, "html-color", "Font Color",null,{tags:['Basic','Shared']});
 
-    CommonXY.prototype.publish("chartScrollbar", false, "boolean", "Chart Scrollbar");
+    CommonXY.prototype.publish("lineWidth", 0, "number", "Line Thickness", null, {min:0,max:10,step:1,inputType:'range',tags:['Basic','Shared']});
+    CommonXY.prototype.publish("lineColor", null, "html-color", "Color of the data/content lines",null,{tags:['Basic','Shared']});
+    CommonXY.prototype.publish("lineOpacity", 0, "number", "Line Opacity", null, {min:0,max:1,step:0.001,inputType:'range',tags:['Basic','Shared']});
 
-    CommonXY.prototype.publish("globalTooltipText","x:[[x]] y:[[y]]", "string", "Tooltip Text"); // x:[[x]] y:[[y]]
-    CommonXY.prototype.publish("graphTooltipText",["x:[[x]] y:[[y]]","x:[[x]] y:[[y]]"], "array", "Tooltip Text"); // [[category]]([[title]]): [[value]
+    CommonXY.prototype.publish("dashedLineStyle", 0, "number", "",null,{tags:['Advanced','Shared']});
 
-    CommonXY.prototype.publish("globalFillAlpha", 0, "number", "Shape Opacity", null, {min:0,max:1,step:0.001,inputType:'range'});
-    CommonXY.prototype.publish("globalLineAlpha", 0, "number", "Line Opacity", null, {min:0,max:1,step:0.001,inputType:'range'});
-    CommonXY.prototype.publish("globalLineThickness", 0, "number", "Line Thickness", null, { min: 0, max: 10, step: 0.1, inputType: 'range' });
-    CommonXY.prototype.publish("globalBulletSize", 8, "number", "Bullet Size");
-    CommonXY.prototype.publish("globalBulletType", "round", "set", "Bullet Type", ["none", "round", "square", "triangleUp", "triangleDown", "triangleLeft", "triangleRight", "bubble", "diamond"]);
+    CommonXY.prototype.publish("yAxisTitle", "Axis title", "string", "Y-Axis Title",null,{tags:['Basic','Shared']});
+    CommonXY.prototype.publish("xAxisTitle", "Axis title", "string", "X-Axis Title",null,{tags:['Basic','Shared']});
 
-    CommonXY.prototype.publish("graphFillAlpha", [], "array", "Area Opacity", null, {min:0, max:1,step:0.001,inputType:'range'});
-    CommonXY.prototype.publish("graphLineAlpha", [], "array", "Area Border Opacity", null, {min:0,max:1,step:0.001,inputType:'range'});
-    CommonXY.prototype.publish("graphLineThickness", [], "array", "Line Thickness", null, {min:0,max:1,step:0.001,inputType:'range'});
-    CommonXY.prototype.publish("graphBulletSize", [], "number", "Bullet Size");
-    CommonXY.prototype.publish("graphBulletType", [], "array", "Bullet Type");
+    CommonXY.prototype.publish("xAxisBaselineColor", null, "html-color", "Axis color",null,{tags:['Basic','Shared']});
+    CommonXY.prototype.publish("yAxisBaselineColor", null, "html-color", "Axis color",null,{tags:['Basic','Shared']});
 
-    CommonXY.prototype.publish("startDuration", 0.3, "number", "Start Duration (sec)");
+    CommonXY.prototype.publish("xAxisFontColor", null, "html-color", "Horizontal axis text style (Color)",null,{tags:['Basic','Shared']});
+    CommonXY.prototype.publish("yAxisFontColor", null, "html-color", "Vertical axis text style (Color)",null,{tags:['Basic','Shared']});
 
-    CommonXY.prototype.publish("dataDateFormat", null, "string", "");
+    CommonXY.prototype.publish("xAxisTitleFontSize", null, "number", "Vertical axis titletext style (Font Size)",null,{tags:['Basic','Shared']});
+    CommonXY.prototype.publish("yAxisTitleFontSize", null, "number", "Vertical axis titletext style (Font Size)",null,{tags:['Intermediate','Shared']});
 
-    CommonXY.prototype.publish("lineColor", null, "html-color", "Color of the data/content lines");
+    CommonXY.prototype.publish("xAxisTitleFontColor", null, "html-color", "Color of axis value labels. Will use chart's color if not set.",null,{tags:['Basic','Shared']});
+    CommonXY.prototype.publish("yAxisTitleFontColor", null, "html-color", "Color of axis value labels. Will use chart's color if not set.",null,{tags:['Basic','Shared']});
+
+    CommonXY.prototype.publish("xAxisLabelRotation", null, "number", "X-Axis Label Rotation", null, {min:0,max:90,step:0.1,inputType:'range',tags:['Intermediate','Shared']});
+
+    CommonXY.prototype.publish("axisLineWidth", 1, "number", "Thickness of axis",null,{tags:['Intermediate','Shared']});
+
+    /**
+     * Publish Params Unique To This Widget
+     */
+    CommonXY.prototype.publish("axisAlpha", 1, "number", "Axis opacity",null,{tags:['Intermediate']});
+
+    CommonXY.prototype.publish("showScrollbar", false, "boolean", "Chart Scrollbar",null,{tags:['Intermediate']});
+
+    CommonXY.prototype.publish("bulletSize", 8, "number", "Bullet Size",null,{tags:['Intermediate']});
+    CommonXY.prototype.publish("bulletType", "round", "set", "Bullet Type", ["none", "round", "square", "triangleUp", "triangleDown", "triangleLeft", "triangleRight", "bubble", "diamond"],{tags:['Intermediate']});
+
+    CommonXY.prototype.publish("marginLeft", 50, "number", "Margin (Left)",null,{tags:['Intermediate']});
+    CommonXY.prototype.publish("marginRight", 10, "number", "Margin (Right)",null,{tags:['Intermediate']});
+    CommonXY.prototype.publish("marginTop", 10, "number", "Margin (Top)",null,{tags:['Intermediate']});
+    CommonXY.prototype.publish("marginBottom", 50, "number", "Margin (Bottom)",null,{tags:['Intermediate']});
+
+    CommonXY.prototype.publish("dataDateFormat", null, "string", "",null,{tags:['Private']});
+
+    CommonXY.prototype.publish("xAxisAutoGridCount", true, "boolean", "Specifies whether number of gridCount is specified automatically, acoarding to the axis size",null,{tags:['Advanced']});
+    CommonXY.prototype.publish("yAxisAutoGridCount", true, "boolean", "Specifies whether number of gridCount is specified automatically, acoarding to the axis size",null,{tags:['Advanced']});
+
+    CommonXY.prototype.publish("xAxisGridPosition", "middle", "set", "Specifies if a grid line is placed on the center of a cell or on the beginning of a cell", ["start","middle"],{tags:['Advanced']});
+    CommonXY.prototype.publish("yAxisGridPosition", "middle", "set", "Specifies if a grid line is placed on the center of a cell or on the beginning of a cell", ["start","middle"],{tags:['Advanced']});
+
+    CommonXY.prototype.publish("xAxisFillAlpha", 0, "number", "Fill opacity. Every second space between grid lines can be filled with color. Set fillAlpha to a value greater than 0 to see the fills.",null,{tags:['Intermediate']});
+    CommonXY.prototype.publish("yAxisFillAlpha", 0, "number", "Fill opacity. Every second space between grid lines can be filled with color. Set fillAlpha to a value greater than 0 to see the fills.",null,{tags:['Intermediate']});
+
+    CommonXY.prototype.publish("xAxisFillColor", null, "html-color", "Fill color. Every second space between grid lines can be filled with color. Set fillAlpha to a value greater than 0 to see the fills.",null,{tags:['Intermediate']});
+    CommonXY.prototype.publish("yAxisFillColor", null, "html-color", "Fill color. Every second space between grid lines can be filled with color. Set fillAlpha to a value greater than 0 to see the fills.",null,{tags:['Intermediate']});
+
+    CommonXY.prototype.publish("xAxisGridAlpha", 0.2, "number", "Grid alpha.",null,{tags:['Intermediate']});
+    CommonXY.prototype.publish("yAxisGridAlpha", 0.2, "number", "Grid alpha.",null,{tags:['Intermediate']});
+
+    CommonXY.prototype.publish("xAxisDashLength", 0, "number", "Length of a dash. 0 means line is not dashed.",null,{tags:['Advanced']});
+    CommonXY.prototype.publish("yAxisDashLength", 0, "number", "Length of a dash. 0 means line is not dashed.",null,{tags:['Advanced']});
+
+    //CommonXY.prototype.publish("yAxisMinimum", null, "number", "",null,{tags:['Intermediate']});
+    CommonXY.prototype.publish("yAxisTitleOffset", null, "number", "",null,{tags:['Intermediate']});
 
     CommonXY.prototype.updateChartOptions = function() {
         var context = this;
 
         this._chart.theme = "none";
         this._chart.type = "xy";
-        this._chart.pathToImages = "//cdn.rawgit.com/cdnjs/cdnjs/master/ajax/libs/amcharts/3.13.0/images/";
+
+        this._chart.color = this.fontColor();
+        this._chart.fontSize = this.fontSize();
+        this._chart.fontFamily = this.fontFamily();
 
         if (this.marginLeft()) { this._chart.marginLeft = this.marginLeft(); }
         if (this.marginRight()) { this._chart.marginRight = this.marginRight(); }
@@ -65,26 +108,48 @@
         this._chart.dataDateFormat = this.dataDateFormat();
 
         this._chart.valueAxes[0].position = "bottom";
-        this._chart.valueAxes[0].axisAlpha = 0;
-        this._chart.valueAxes[0].dashLength = 1;
+        this._chart.valueAxes[0].axisAlpha = this.axisAlpha();
         this._chart.valueAxes[0].title = this.xAxisTitle();
+        this._chart.valueAxes[0].axisThickness = this.axisLineWidth();
+        this._chart.valueAxes[0].axisColor = this.xAxisBaselineColor();
+        this._chart.valueAxes[0].color = this.xAxisFontColor();
+        this._chart.valueAxes[0].titleFontSize = this.xAxisTitleFontSize();
+        this._chart.valueAxes[0].titleColor = this.xAxisTitleFontColor();
+        this._chart.valueAxes[0].labelRotation = this.xAxisLabelRotation();
+        this._chart.valueAxes[0].autoGridCount = this.xAxisAutoGridCount();
+        this._chart.valueAxes[0].gridPosition = this.xAxisGridPosition();
+        this._chart.valueAxes[0].fillAlpha = this.xAxisFillAlpha();
+        this._chart.valueAxes[0].fillColor = this.xAxisFillColor();
+        this._chart.valueAxes[0].gridAlpha = this.xAxisGridAlpha();
+        this._chart.valueAxes[0].dashLength = this.xAxisDashLength();
 
         this._chart.valueAxes[1].position = "left";
-        this._chart.valueAxes[1].axisAlpha = 0;
-        this._chart.valueAxes[1].dashLength = 1;
+        this._chart.valueAxes[1].axisAlpha = this.axisAlpha();
         this._chart.valueAxes[1].title = this.yAxisTitle();
+        this._chart.valueAxes[1].axisThickness = this.axisLineWidth();
+        this._chart.valueAxes[1].axisColor = this.yAxisBaselineColor();
+        this._chart.valueAxes[1].color = this.yAxisFontColor();
+        this._chart.valueAxes[1].titleFontSize = this.yAxisTitleFontSize();
+        this._chart.valueAxes[1].titleColor = this.yAxisTitleFontColor();
+        this._chart.valueAxes[1].autoGridCount = this.yAxisAutoGridCount();
+        this._chart.valueAxes[1].gridPosition = this.yAxisGridPosition();
+        this._chart.valueAxes[1].fillAlpha = this.yAxisFillAlpha();
+        this._chart.valueAxes[1].fillColor = this.yAxisFillColor();
+        this._chart.valueAxes[1].gridAlpha = this.yAxisGridAlpha();
+        this._chart.valueAxes[1].dashLength = this.yAxisDashLength();
+        this._chart.valueAxes[1].axisTitleOffset = this.yAxisTitleOffset();
 
         // DataProvider
-        this._chart.dataProvider = this.formatData(this._data); 
+        this._chart.dataProvider = this.formatData(this._data);
 
         this._chart.dataProvider.forEach(function(dataPoint,i){
             context._chart.dataProvider[i].color = context._palette(dataPoint[context._columns[2]]); // By Y value
             context._chart.dataProvider[i].linecolor = context.lineColor() !== null ? context.lineColor() : context._palette(dataPoint[context._columns[2]]);
         });
         this._chart.colors = [];
-            
+
         // Scroll Bar
-        if (this.chartScrollbar()) {
+        if (this.showScrollbar()) {
             this._chart.chartScrollbar.enabled = true;
         } else {
             this._chart.chartScrollbar.enabled = false;
@@ -95,27 +160,30 @@
 
     CommonXY.prototype.buildGraphObj = function(gType,i) {
         var context = this;
-        var gObj = {}; 
+        var gObj = {};
 
-        gObj.balloonText = context.graphTooltipText()[i] || context.globalTooltipText();
-        gObj.fillAlphas = context.graphFillAlpha()[i] || context.globalFillAlpha();
-        gObj.lineAlpha = context.graphLineAlpha()[i] || context.globalLineAlpha();
-        gObj.lineThickness = context.graphLineThickness()[i] || context.globalLineThickness();
-        gObj.bullet = context.graphBulletType()[i] || context.globalBulletType();
-        gObj.bulletSize = context.graphBulletSize()[i] || context.globalBulletSize();
-        
+        gObj.balloonText = context.tooltipTemplate();
+        gObj.lineAlpha = context.lineOpacity();
+        gObj.lineThickness = context.lineWidth();
+        gObj.bullet = context.bulletType();
+        gObj.bulletSize = context.bulletSize();
+        gObj.dashLength = context.dashedLineStyle(); // TODO: convert to css Array Prop
+        gObj.lineAlpha = context.lineOpacity();
+        gObj.lineColor = context.lineColor();
+        gObj.lineThickness = context.lineWidth();
+
         gObj.type = gType;
 
         gObj.colorField = "color";
         gObj.lineColorField = "linecolor";
-        
+
         // XY Values
         gObj.xField = context._columns[1];
         gObj.yField = context._columns[2];
-        
+
         return gObj;
-    };    
-    
+    };
+
     CommonXY.prototype.formatData = function(dataArr) {
         var context = this;
         var dataObjArr = [];
@@ -151,9 +219,9 @@
             theme: "none",
             type: "xy",
             pathToImages: "//www.amcharts.com/lib/3/images/",
-            automargins: false,        
+            automargins: false,
             chartScrollbar: {},
-            valueAxes: [       
+            valueAxes: [
                 {position:"bottom",title:" "},
                 {position:"left",title:" "}
             ],
@@ -163,17 +231,17 @@
                 enabled: true
             }
         };
-        
-        this._chart = AmCharts.makeChart(domNode, initObj);        
+
+        this._chart = AmCharts.makeChart(domNode, initObj);
     };
 
     CommonXY.prototype.update = function(domNode, element) {
         HTMLWidget.prototype.update.apply(this, arguments);
 
         domNode.style.width = this.size().width + 'px';
-        domNode.style.height = this.size().height + 'px'; 
+        domNode.style.height = this.size().height + 'px';
 
-        this._palette = this._palette.switch(this.paletteID()); 
+        this._palette = this._palette.switch(this.paletteID());
     };
 
     return CommonXY;
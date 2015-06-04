29ad09ca97924d513c34ab8c1cf4600d9ce74a17
Mathew Zummo <mzummo@gmail.com>
2015-06-04 09:03:37 -0400

    GH-296 Add buttons to SVG Surface
    Allow user to specify an array of Icons to appear in the title.
    Fix click notification for icons.
    
    Fixes #296
    
    Signed-off-by: Mathew Zummo <mzummo@gmail.com>

--- src/common/Surface.js
+++ src/common/Surface.js
@@ -1,11 +1,11 @@
 "use strict";
 (function (root, factory) {
     if (typeof define === "function" && define.amd) {
-        define(["./SVGWidget", "./Icon", "./Shape", "./Text", "./FAChar", "./Menu", "css!./Surface"], factory);
+        define(["d3", "./SVGWidget", "./Icon", "./Shape", "./Text", "./FAChar", "./Menu", "css!./Surface"], factory);
     } else {
-        root.common_Surface = factory(root.common_SVGWidget, root.common_Icon, root.common_Shape, root.common_Text, root.common_FAChar, root.common_Menu);
+        root.common_Surface = factory(root.d3, root.common_SVGWidget, root.common_Icon, root.common_Shape, root.common_Text, root.common_FAChar, root.common_Menu);
     }
-}(this, function (SVGWidget, Icon, Shape, Text, FAChar, Menu) {
+}(this, function (d3, SVGWidget, Icon, Shape, Text, FAChar, Menu) {
     function Surface() {
         SVGWidget.call(this);
 
@@ -14,6 +14,7 @@
             .faChar("\uf07b")
             .paddingPercent(50)
         ;
+
         this._container = new Shape()
             .class("container")
             .shape("rect")
@@ -42,18 +43,22 @@
 
         this._showContent = true;
         this._content = null;
+        this._surfaceButtons = [];
     }
     Surface.prototype = Object.create(SVGWidget.prototype);
     Surface.prototype._class += " common_Surface";
 
-    Surface.prototype.publish("showTitle", true, "boolean", "Show Title",null,{tags:['Private']});
-    Surface.prototype.publish("title", "", "string", "Title",null,{tags:['Private']});
+    Surface.prototype.publish("showTitle", true, "boolean", "Show Title",null,{tags:["Basic"]});
+    Surface.prototype.publish("title", "", "string", "Title",null,{tags:["Basic"]});
     Surface.prototype.publishProxy("titleFontSize", "_text", "fontSize");
-    Surface.prototype.publish("showIcon", true, "boolean", "Show Title",null,{tags:['Private']});
+    Surface.prototype.publish("showIcon", true, "boolean", "Show Title",null,{tags:["Advance"]});
     Surface.prototype.publishProxy("icon_faChar", "_icon", "faChar");
     Surface.prototype.publishProxy("icon_shape", "_icon", "shape");
     //Surface.prototype.publish("menu");
-    Surface.prototype.publish("content", null, "widget", "Content",null,{tags:['Private']});
+    Surface.prototype.publish("content", null, "widget", "Content",null,{tags:["Private"]});
+
+    Surface.prototype.publish("buttonAnnotations", [], "array", "Button Array",null,{tags:["Intermediate"]});
+    Surface.prototype.publish("buttonGutter", 20, "number", "Space Between Menu and Buttons",null,{tags:["Intermediate"]});
 
     Surface.prototype.menu = function (_) {
         if (!arguments.length) return this._menu.data();
@@ -79,6 +84,8 @@
     Surface.prototype.testData = function () {
         this.title("Hello and welcome!");
         this.menu(["aaa", "bbb", "ccc"]);
+        this.buttonAnnotations([{id:"button_1",char:"\uf010",shape:"square",diameter:14,padding:10}, {id:"button_2",char:"\uf00e",shape:"square",diameter:14,padding:10}]);
+
         return this;
     };
 
@@ -116,6 +123,7 @@
 
     Surface.prototype.update = function (domNode, element) {
         SVGWidget.prototype.update.apply(this, arguments);
+        var context = this;
 
         this._icon
             .display(this.showTitle() && this.showIcon())
@@ -130,13 +138,43 @@
             .display(this.showTitle())
             .render()
         ;
+
+        var surfaceButtons = element.selectAll(".surface-button").data(this.buttonAnnotations());
+        surfaceButtons.enter().append("g").classed("surface-button",true)
+            .each(function (button, idx) {
+                context._surfaceButtons[idx] = new Icon()
+                    .faChar(button.char)
+                    .diameter(button.diameter)
+                    .image_colorFill(button.image_colorFill)
+                    .shape_colorFill(button.shape_colorFill)
+                    .shape_colorStroke(button.shape_colorStroke)
+                    .paddingPercent(button.padding)
+                    .shape(button.shape)
+                    .target(this)
+                    .display(context.showTitle())
+                    .render(function(widget) {
+                        widget.element().style("cursor","pointer");
+                        widget.click = function(d) { context.click(d,widget); };
+                    })
+                ;
+            })
+        ;
+        surfaceButtons.exit()
+            .each(function (d, idx) {
+                var element = d3.select(this);
+                delete context._surfaceButtons[idx];
+                element.remove();
+            })
+        ;
+
+        var buttonClientHeight = this.showTitle() ? Math.max.apply(null,this._surfaceButtons.map(function(d) { return d.getBBox(true).height; })) : 0;
         var iconClientSize = this.showIcon() ? this._icon.getBBox(true) : {width:0, height: 0};
         var textClientSize = this._text.getBBox(true);
         var menuClientSize = this._menu.getBBox(true);
-        var titleRegionHeight = Math.max(iconClientSize.height, textClientSize.height, menuClientSize.height);
+        var titleRegionHeight = Math.max(iconClientSize.height, textClientSize.height, menuClientSize.height, buttonClientHeight);
         var yTitle = (-this._size.height + titleRegionHeight) / 2;
 
-        var titleTextHeight = Math.max(textClientSize.height, menuClientSize.height);
+        var titleTextHeight = Math.max(textClientSize.height, menuClientSize.height, buttonClientHeight);
 
         var topMargin = titleRegionHeight <= titleTextHeight ? 0 : (titleRegionHeight - titleTextHeight) / 2;
         var leftMargin = topMargin;
@@ -157,6 +195,10 @@
         this._text
             .move({ x: (iconClientSize.width / 2 - menuClientSize.width / 2) / 2, y: yTitle })
         ;
+        this._surfaceButtons.forEach(function(button, idx) {
+            button.move({ x: (context._size.width / 2 - menuClientSize.width * 2) - context.buttonGutter() - button.getBBox(true).width * idx, y: yTitle });
+        });
+
         if (this.showTitle()) {
             this._container
                 .pos({ x: leftMargin / 2, y: titleRegionHeight / 2 - topMargin / 2 })
@@ -176,13 +218,13 @@
         if (this._showContent) {
             var xOffset = leftMargin;
             var yOffset = titleRegionHeight - topMargin;
-            var context = this;
             var content = element.selectAll(".content").data(this.content() ? [this.content()] : [], function (d) { return d._id; });
             content.enter().append("g")
                 .attr("class", "content")
                 .attr("clip-path", "url(#" + this.id() + "_clip)")
                 .each(function (d) {
                     d.target(this);
+                    // TODO: buttons within container
                 })
             ;
             content
@@ -216,7 +258,7 @@
             ;
         }
 
-        this._menu.element().node().parentNode.appendChild(this._menu.element().node());
+        this._menu.element().node().parentNode.appendChild(this._menu.element().node()); // Make sure menu is on top (Z-Order POV)
     };
 
     Surface.prototype.exit = function (domNode, element) {
@@ -263,6 +305,10 @@
             }
         });
         return nearest && nearest.i ? nearest.i : null;
+    };
+
+    Surface.prototype.click = function(obj, widget) {
+        console.log("Clicked: " + obj.id);
     };
 
     return Surface;

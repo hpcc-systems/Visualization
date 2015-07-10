7f4027a170d24666b71a11c35ef00df83eb0eae5
Gordon Smith <gordon.smith@lexisnexis.com>
2015-07-10 22:12:41 +0100

    Release 1.2.4

---
+++ dist-amd/common/IList.js
@@ -0,0 +1,1 @@
+(function(e,t){typeof define=="function"&&define.amd?define([],t):e.common_IList=t()})(this,function(){function e(){}return e.prototype.testData=function(){var e=["This","is a","list","of some text."];return this.data(e),this},e.prototype.click=function(e){console.log("Click:  "+e)},e});

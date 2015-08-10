5af96a2775ce7e50a1a5fdd1fbb893ba690543e8
Anmol Jagetia <anmoljagetia@hotmail.com>
2015-08-10 13:46:35 +0530

    Adds Null Value test

--- test/widgets.js
+++ test/widgets.js
@@ -18,6 +18,34 @@ define(["require"], function (require) {
                         done();
                     });
                 });
+<<<<<<< HEAD
+=======
+
+                it ("Adding widget with loadData method", function (done) {
+                    var individualDiv = document.createElement("div");
+                    individualDiv.setAttribute('id', path);
+                    individualDiv.setAttribute('class', 'widgetElement');
+                    var element = document.getElementById("testWidget");
+                    element.appendChild(individualDiv);
+                    element.appendChild(document.createElement("br"));
+                    require([path], function (Widget) {
+                        var vizWidget = new Widget()
+                            .target(path)
+                            .testData()
+                            .render()
+                        ;
+                        done();
+                    });
+                });
+
+                it ("Removing widget from the page", function (done) {
+                    var e = document.getElementById(path);
+                    e.parentNode.removeChild(e);
+                    var f = document.getElementById(path);
+                    assert.isNull(f, 'element removed and there are no errors');
+                    done();
+                });
+>>>>>>> dcc34a6... Adds Null Value test
             });
         });
     });

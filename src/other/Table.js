d9dc38bfc7d146229e6d6d298138fe845d3a2c9e
Mathew Zummo <mzummo@gmail.com>
2015-08-13 13:23:28 -0400

    GH-705 Fix table pagination Fix table pagination Fixes GH-705
    Signed-off-by: Mathew Zummo <mzummo@gmail.com>

--- src/other/Table.js
+++ src/other/Table.js
@@ -85,12 +85,12 @@
         if (this._paginator.numItems() === 0) { // only run on first render
             this._paginator.numItems(1);
             this.itemsPerPage(1);
-            this._paginator.render();
         }
-
+        this._paginator.render();
         var thHeight = this.calcHeight(th);
         var tcellHeight = this.calcHeight(this._generateTempCell());
         var paginatorHeight = this.calcHeight(this._paginator.element());
+
         var ipp = Math.ceil((this.height() - thHeight - paginatorHeight) / tcellHeight) || 1;
         return ipp;
     };

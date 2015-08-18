ad2ebc8499794d11c061af0c62ede0fcecae1198
Anmol Jagetia <--set>
2015-08-18 16:56:56 +0530

    Adds unit tests for Table Widget
    Signed-off-by: Anmol Jagetia <anmoljagetia@gmail.com>

--- test/anmol.js
+++ test/anmol.js
@@ -55,7 +55,79 @@ define(["d3", "src/form/Slider", "src/common/Icon", "src/other/Table", "src/othe
                 assert.equal(widgetDiv.html(), "");
                 done();
             });
+        });
 
+        describe("Table Tests", function () {
+            var widget = { path: "src/other/Table" };
+            var element = d3.select("#testWidget");
+            var vizWidget, widgetDiv, testDiv;
+
+            it("Adding widget and making sure if all the components are loaded properly", function (done) {
+                this.timeout(5000);
+                testDiv = element.append("div")
+                    .attr("class", "widgetTest")
+                ;
+                widgetDiv = testDiv.append("div")
+                    .attr("class", "widget " + widget.path)
+                ;
+                testDiv.append("center")
+                    .attr("class", "title")
+                    .text(widget.path)
+                ;
+                vizWidget = new Table()
+                    .target(widgetDiv.node())
+                    .columns(["Alphabets"])
+                    .data(["d", "c", "e", "a", "f", "g", "z", "x", "y"])
+                    .render()
+                ;
+                assert.instanceOf(vizWidget._paginator, Paginator, "paginator exists and is an instance of Paginator");
+                done();
+            });
+
+            it("Checking paginator functionality", function (done) {
+                var page = d3.select(".paginator");
+                assert.isNull(page.node(), "paginator doesn't exist");
+                vizWidget.pagination(true).render();
+                page = d3.select(".paginator");
+                assert.isNotNull(page.node(), "paginator exists");
+                done();
+            });
+
+            it("Table sorting", function (done) {
+                var list = d3.selectAll(".thText");
+                assert.equal(vizWidget.data()[0], "d", "initially the first char is d");
+                // list.node().click();
+                clickElement(list[0][list[0].length - 1], function () {
+                    assert.equal(vizWidget.data()[0], "a", "now it is a");
+                });
+
+                clickElement(list[0][list[0].length - 1], function () {
+                    assert.equal(vizWidget.data()[0], "z", "now it is z");
+                    done();
+                });
+
+                 function clickElement(el, callback) {
+                    var ev = document.createEvent("MouseEvent");
+                    ev.initMouseEvent(
+                      "click",
+                      true /* bubble */, true /* cancelable */,
+                      window, null,
+                      0, 0, 0, 0, /* coordinates */
+                      false, false, false, false, /* modifier keys */
+                      0 /*left*/, null
+                    );
+                    el.dispatchEvent(ev);
+                    callback();
+                };
+
+            });
+
+            it("Removing the table widget from the page", function (done) {
+                assert.notEqual(widgetDiv.html(), "");
+                vizWidget.target(null);
+                assert.equal(widgetDiv.html(), "");
+                done();
+            });
         });
     });
 });

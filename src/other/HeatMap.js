1e6a02b8a11daa4b8732f16df454058fb9d77fba
Jay Brundage <jaman.brundage@lexisnexis.com>
2015-10-26 16:48:23 -0400

    GH-1088 Adds HeatMap skew and color controls
    Fixes GH-1088
    
    Signed-off-by: Jay Brundage <jaman.brundage@lexisnexis.com>

--- src/other/HeatMap.js
+++ src/other/HeatMap.js
@@ -1,22 +1,35 @@
 "use strict";
 (function (root, factory) {
     if (typeof define === "function" && define.amd) {
-        define(["d3", "../common/CanvasWidget", "simpleheat"], factory);
+        define(["d3", "../common/CanvasWidget", "simpleheat", "../common/Palette"], factory);
     } else {
-        root.other_HeatMap = factory(root.d3, root.common_CanvasWidget, root.simpleheat);
+        root.other_HeatMap = factory(root.d3, root.common_CanvasWidget, root.simpleheat, root.common_Palette);
     }
-}(this, function (d3, CanvasWidget, simpleheat) {
+}(this, function (d3, CanvasWidget, simpleheat, Palette) {
     function HeatMap() {
         CanvasWidget.call(this);
     }
     HeatMap.prototype = Object.create(CanvasWidget.prototype);
     HeatMap.prototype.constructor = HeatMap;
+    
+    HeatMap.prototype._palette = Palette.rainbow("default");
     HeatMap.prototype._class += " other_HeatMap";
     
-    HeatMap.prototype.publish("radius", 25, "number", "Set point radius", null, { tags: ["Basic"] });
+    HeatMap.prototype.publish("radius", 15, "number", "Set point radius", null, { tags: ["Basic"] });
     HeatMap.prototype.publish("blur", 15, "number", "Set point blur", null, { tags: ["Basic"] });
     HeatMap.prototype.publish("max", 1, "number", "Set max data value", null, { tags: ["Basic"] });
-    HeatMap.prototype.publish("gradient", [], "array", "Set gradient colors", null, { tags: ["Basic"] });
+    
+    HeatMap.prototype.publish("gradient", {0.4:"blue",0.6:"cyan",0.7:"lime",0.8:"yellow",1.0:"red"}, "object", "Set gradient colors", null, { tags: ["Basic"] });
+
+    HeatMap.prototype.publish("usePalette", false, "boolean", "If true, uses paletteID and colorCount to determine gradient",null,{tags:["Basic"]});
+
+    HeatMap.prototype.publish("colorCount", 10, "number", "Top left x-value",null,{tags:["Basic"]});
+    HeatMap.prototype.publish("paletteID", "default", "set", "Palette ID", HeatMap.prototype._palette.switch(), {tags:["Basic"]});
+
+    HeatMap.prototype.publish("topLeftX", null, "number", "Top left x-value",null,{tags:["Basic"]});
+    HeatMap.prototype.publish("topLeftY", null, "number", "Top left y-value",null,{tags:["Basic"]});
+    HeatMap.prototype.publish("bottomRightX", null, "number", "Bottom right x-value",null,{tags:["Basic"]});
+    HeatMap.prototype.publish("bottomRightY", null, "number", "Bottom right y-value",null,{tags:["Basic"]});
 
     HeatMap.prototype.enter = function (domNode, element) {
         CanvasWidget.prototype.enter.apply(this, arguments);
@@ -27,13 +40,30 @@
 
     HeatMap.prototype.update = function (domNode, element) {
         CanvasWidget.prototype.update.apply(this, arguments);
-        if (this.data().length === 4) {
-            this.data().map(function(a) { return a.splice(0,3); });
+        
+        this._palette = this._palette.switch(this.paletteID());
+        
+        if(this.topLeftX() !== null && this.topLeftY() !== null && this.bottomRightX() !== null && this.bottomRightY() !== null){
+            this._heat.data(this.skewedData());
+        } else {
+            this._heat.data(this.data());
         }
-        this._heat.data(this.data());
+        
 
         if(this.radius()){
             this._heat.radius(this.radius(), this.blur());
+        }
+        if(this.usePalette()){
+            var grad = {};
+            for(var idx = 1;idx<=this.colorCount();idx++){
+                var value = idx/this.colorCount();
+                grad[value] = this._palette(idx,1,this.colorCount());
+            }
+            this._heat.defaultGradient = grad;
+            this._heat.gradient(grad);
+        }else if(this.gradient()){
+            this._heat.defaultGradient = this.gradient();
+            this._heat.gradient(this.gradient());
         }
 
         this._heat.draw();    
@@ -42,6 +72,38 @@
     HeatMap.prototype.exit = function(domNode, element) {
         delete this._heat;
         CanvasWidget.prototype.exit.apply(this, arguments);
+    };
+    
+    HeatMap.prototype.resize = function(size) {
+        CanvasWidget.prototype.resize.apply(this, arguments);
+        if(this._heat !== undefined){
+            this._heat.resize();
+        }
+    };
+    
+    HeatMap.prototype.skewedData = function() {
+        var context = this;
+        var retArr = [];
+        var arr = this.data();
+        var box = this.element().node().getBoundingClientRect();
+        
+        var coordsWidth = this.bottomRightX() - this.topLeftX();
+        var coordsHeight = this.bottomRightY() - this.topLeftY();
+        
+        var pixelValueX = coordsWidth / box.width;
+        var pixelValueY = coordsHeight / box.height;
+        
+        arr.forEach(function(n){
+            var left = Math.abs(n[0] - context.topLeftX());
+            var top = Math.abs(n[1] - context.topLeftY());
+            
+            var newX = left / pixelValueX;
+            var newY = top / pixelValueY;
+            
+            retArr.push([newX,newY,n[2]]);
+        });
+        
+        return retArr;
     };
  
     return HeatMap;
--- test/otherFactory.js
+++ test/otherFactory.js
@@ -15,6 +15,59 @@
                         .data(DataFactory.HeatMap.simple.data)
                     );
                 });
+            },
+            skew: function (callback) {
+                require(["test/DataFactory", "src/other/HeatMap"], function (DataFactory, HeatMap) {
+                    callback(new HeatMap()
+                        .columns(DataFactory.HeatMap.skew.columns)
+                        .data(DataFactory.HeatMap.skew.data)
+                        .topLeftX(-10000).topLeftY(-10000)
+                        .bottomRightX(10000).bottomRightY(10000)
+                    );
+                });
+            },
+            rapidInterval: function (callback) {
+                require(["test/DataFactory", "src/other/HeatMap"], function (DataFactory, HeatMap) {
+                    var heat = new HeatMap().columns(DataFactory.HeatMap.center.columns).data(DataFactory.HeatMap.center.data)
+                        .topLeftX(-10000).topLeftY(-10000)
+                        .bottomRightX(10000).bottomRightY(10000)
+                        .radius(13).blur(5)
+                    ;
+                    var step = 400;
+                    var renderTimeLimit = 2000;
+                    var mitosisLoop = setInterval(function(){
+                        var prevRenderTime = new Date().getTime();
+                        var newData = [];
+                        heat.data().forEach(function(n){
+                            var splitChance = 1/(heat.data().length/20);
+                            var rand = Math.random();
+                            if(rand <= splitChance * n[2] * 5){
+                                _split(n);
+                            } else if (rand <= splitChance * 100) {
+                                _move(n);
+                            } else {
+                                n[2] = rand >= 0.999999 ? 1 : n[2];
+                                newData.push(n);
+                            }
+                        });
+                        heat.data(newData).render();
+                        if(new Date().getTime() - prevRenderTime > renderTimeLimit){
+                            clearInterval(mitosisLoop);
+                        }
+                        function _split(c){
+                            var o = step + (Math.random()*step);
+                            newData.push(c);
+                            newData.push([[c[0],c[1]-o,c[2]],[c[0]+o,c[1],c[2]],[c[0],c[1]+o,c[2]],[c[0]-o,c[1],c[2]]][Math.floor(Math.random()*4)]);
+                        }
+                        function _move(cell){
+                            var movedCell = [cell[0] + (step*Math.random()*[-1,1][Math.floor(Math.random()*2)]),cell[1] + (step*Math.random()*[-1,1][Math.floor(Math.random()*2)]),cell[2] * 0.99];
+                            if(movedCell[2] > 0.01){
+                                newData.push(movedCell);
+                            }
+                        }
+                    },100);
+                    callback(heat);
+                });
             }
         },
         Image: {

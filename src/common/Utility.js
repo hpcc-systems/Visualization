a325d6a0fdec73aa80fd40eb74ca3ea79a8d0545
Mathew Zummo <mzummo@gmail.com>
2015-11-04 16:45:02 -0500

    changes
    Signed-off-by: Mathew Zummo <mzummo@gmail.com>

--- src/common/Utility.js
+++ src/common/Utility.js
@@ -143,13 +143,12 @@
 
     return {
         hash: function(str) {
-            var hash = 0;
-            var char = undefined;
-            if (str.length === 0) return hash;
-            for (var i = 0; i < str.length; i++) {
-                char = str.charCodeAt(i);
-                hash = ((hash<<5)-hash)+char;
-                hash = hash & hash; // Convert to 32bit integer
+            var hash = 0, i, chr, len;
+            if (str.length == 0) return hash;
+            for (i = 0, len = str.length; i < len; i++) {
+                chr   = str.charCodeAt(i);
+                hash  = ((hash << 5) - hash) + chr;
+                hash |= 0; // Convert to 32bit integer
             }
             return hash;
         },

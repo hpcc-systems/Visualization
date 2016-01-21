c05ca88a81a640f29f36004caf88868df6fabd90
Gordon Smith <gordon.smith@lexisnexis.com>
2016-01-21 07:55:45 +0000

    GH-1370 MegaChart missing click handler
    The marshaller won't attach a click handler, if the chart does not have a default one already.
    
    Signed-off-by: Gordon Smith <gordon.smith@lexisnexis.com>

--- src/composite/MegaChart.js
+++ src/composite/MegaChart.js
@@ -152,5 +152,10 @@
         return obj;
     };
 
+    //  Events  ---
+    MegaChart.prototype.click = function (row, column, selected) {
+        console.log("Click:  " + JSON.stringify(row) + ", " + column + ", " + selected);
+    };
+
     return MegaChart;
 }));

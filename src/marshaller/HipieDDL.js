974677015787f29ca494818530057c6265f7acd2
Gordon Smith <gordonjsmith@gmail.com>
2016-02-08 15:22:07 +0000

    GH-1469 Optionally allow clear to propagate
    Fixes GH-1469
    
    Signed-off-by: Gordon Smith <gordonjsmith@gmail.com>

--- src/marshaller/HipieDDL.js
+++ src/marshaller/HipieDDL.js
@@ -820,7 +820,7 @@
             this.widget.data([]);
             this.source.getOutput().request = {};
         }
-        if (this._eventValues) {
+        if (this.dashboard.marshaller.propogateClear() && this._eventValues) {
             delete this._eventValues;
             this.events.getUpdatesVisualizations().forEach(function (updatedViz) {
                 updatedViz.clear();
@@ -1120,6 +1120,7 @@
     function Marshaller() {
         this._proxyMappings = {};
         this._widgetMappings = d3.map();
+        this._propogateClear = false;
     }
 
     Marshaller.prototype.commsDataLoaded = function () {
@@ -1195,6 +1196,12 @@
     Marshaller.prototype.widgetMappings = function (_) {
         if (!arguments.length) return this._widgetMappings;
         this._widgetMappings = _;
+        return this;
+    };
+
+    Marshaller.prototype.propogateClear = function (_) {
+        if (!arguments.length) return this._propogateClear;
+        this._propogateClear = _;
         return this;
     };
 

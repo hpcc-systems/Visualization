474566ab86472b0c6b23537134036b92814cd113
Gordon Smith <gordonjsmith@gmail.com>
2016-05-12 15:01:09 -0400

    Workshop sample data Boilerplate widgets
    Signed-off-by: Gordon Smith <gordonjsmith@gmail.com>

---
+++ test/templatesFactory.js
@@ -0,0 +1,32 @@
+﻿"use strict";
+(function (root, factory) {
+    if (typeof define === "function" && define.amd) {
+        define([], factory);
+    } else {
+        root.test_templatesFactory = factory();
+    }
+}(this, function () {
+    return {
+        SVG: {
+            simple: function (callback) {
+                require(["test/DataFactory", "templates/SVGTemplate"], function (DataFactory, SVGTemplate) {
+                    callback(new SVGTemplate()
+                        .columns(DataFactory.ND.subjects.columns)
+                        .data(DataFactory.ND.subjects.data)
+                    );
+                });
+            }
+        },
+        HTML: {
+            simple: function (callback) {
+                require(["test/DataFactory", "templates/HTMLTemplate"], function (DataFactory, SVGTemplate) {
+                    callback(new SVGTemplate()
+                        .columns(DataFactory.ND.subjects.columns)
+                        .data(DataFactory.ND.subjects.data)
+                    );
+                });
+            }
+        }
+
+    };
+}));

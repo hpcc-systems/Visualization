6c409769e40b8be5d874c4bb877da4378090ca72
mounicarachakonda <mounica.rachakonda@lexisnexis.com>
2016-11-01 13:37:15 -0400

    Added Handson and Opportunity to multichart

--- src/chart/MultiChart.js
+++ src/chart/MultiChart.js
@@ -83,6 +83,8 @@
         { id: "TABLE_BULLET", display: "Table driven bullet chart", widgetClass: "chart_Bullet" },
         { id: "TABLE_SELECT", display: "Table driven select", widgetClass: "other_Select" },
         { id: "TABLE_AUTOCOMPLETE", display: "Table driven auto complete", widgetClass: "other_AutoCompleteText" },
+        { id: "TABLE_HANDSON", display: "Table driven handson", widgetClass: "handson_Table" },
+        { id: "TABLE_OPPORTUNITY", display: "Table driven opportunity widget", widgetClass: "graph_Opportunity" },
         { id: "TABLE_TREE", display: "Table driven tree", widgetClass: "tree_Dendrogram" },
         { id: "TABLE_TREEMAP", display: "Table driven Treemap", widgetClass: "tree_Treemap" },
         { id: "TABLE_SANKEY", display: "Table driven Sankey", widgetClass: "graph_Sankey" },

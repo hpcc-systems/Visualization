GordonSmith/hpcc-js

GH-2061 Add safe stringify to Utility
Fixes GH-2061

Signed-off-by: Gordon Smith <gordonjsmith@gmail.com>

src/common/Utility.js
@@ -420,6 +420,18 @@
                 }
                 return dest;
             }
+        },
+        stringify: function(obj) {
+            var cache = [];
+            return JSON.stringify(obj, function (key, value) {
+                if (typeof value === 'object' && value !== null) {
+                    if (cache.indexOf(value) !== -1) {
+                        return;
+                    }
+                    cache.push(value);
+                }
+                return value;
+            });
         }
     };
 }));

hpcc-systems/Visualization

src/common/Utility.js
@@ -420,6 +420,18 @@
                 }
                 return dest;
             }
+        },
+        logStringify: function (obj) {
+            var cache = [];
+            return JSON.stringify(obj, function (key, value) {
+                if (typeof value === 'object' && value !== null) {
+                    if (cache.indexOf(value) !== -1) {
+                        return;
+                    }
+                    cache.push(value);
+                }
+                return value;
+            });
         }
     };
 }));
ed7ada53e378d0eb3c7fed13f2a71e7746255ec5
Jay Brundage <jaman.brundage@lexisnexis.com>
2018-01-22 12:16:48 -0500

    Adds Modal buttons to ChartPanel (WIP)

--- packages/layout/src/ChartPanel.ts
+++ packages/layout/src/ChartPanel.ts
@@ -1,17 +1,108 @@
-import { publish, publishProxy, Utility, Widget } from "@hpcc-js/common";
+import { publish, publishProxy, TextBox as Text, Utility, Widget } from "@hpcc-js/common";
 import { Border2 } from "./Border2";
 import { Legend } from "./Legend";
-import { Button, IClickHandler, Item, Spacer, TitleBar, ToggleButton } from "./TitleBar";
+import { Modal } from "./Modal";
+import { Button, IClickHandler, Item, TitleBar, ToggleButton } from "./TitleBar";
 
 export class ChartPanel extends Border2 implements IClickHandler {
 
-    private _toggleLegend: ToggleButton = new ToggleButton(this, "fa-info").selected(false);
-    private _buttonDownload: Button = new Button(this, "fa-download");
-
     private _titleBar = new TitleBar();
+    private _modal = new Modal();
 
     private _legend = new Legend(this);
-
+    @publish([
+        {
+            icon: "fa-filter",
+            type: "ToggleButton",
+            selected: false,
+            init(btn) {
+                if (!window.g_form) window.g_form = new Text().text("Placeholder for a form widget");
+                this._modal
+                    .title("Filter Modal")
+                    .show(true)
+                    .widget(window.g_form)
+                    .render(n => {
+                        n.resize().render();
+                    });
+                this._modal._close = function () {
+                    btn.selected(false).render();
+                };
+            },
+            on(btn) {
+                this._modal.show(true).showPreview(false).render(n => {
+                    n.resize().render();
+                });
+            },
+            off(btn) {
+                this._modal.show(false).render();
+            },
+            mouseenter(btn) {
+                this._modal.showPreview(true).render(n => {
+                    n.resize().render();
+                });
+            },
+            mouseleave(btn) {
+                if (this._modal.showPreview()) {
+                    this._modal.show(false).showPreview(false).render();
+                }
+            },
+        },
+        {
+            icon: "fa-info-circle",
+            type: "ToggleButton",
+            selected: false,
+            on(btn) {
+                this._modal
+                    .title("Filter Modal")
+                    .widget(new Text().text("Info about this ChartPanel"))
+                    .show(true)
+                    .render(n => {
+                        n.resize().render(n => {
+                            n._widget.resize().render();
+                        });
+                    });
+                this._modal._close = function () {
+                    btn.selected(false).render();
+                };
+            },
+            off(btn) {
+                this._modal.show(false).render();
+            },
+        },
+        {
+            icon: "fa-download",
+            type: "Button",
+            selected: false,
+            click(btn) {
+                this.downloadCSV();
+            }
+        },
+        {
+            icon: "fa-list-ul",
+            type: "ToggleButton",
+            selected: false,
+            on(d) {
+                this._legend.visible(true);
+                this.render();
+            },
+            off(d) {
+                this._legend.visible(false);
+                this.render();
+            },
+            update(d) {
+                if (this._prevChartDataFamily !== "ND") {
+                    this._prevChartDataFamily = "ND";
+                    switch (this._prevChartDataFamily) {
+                        case "any":
+                            d.ref.selected(false);
+                            this._legend.visible(false);
+                            break;
+                    }
+                }
+            }
+        },
+    ], "array", "Buttons")
+    button_json: any;
     @publishProxy("_titleBar", undefined, undefined, { reset: true })
     title: publish<this, string>;
     @publish(null, "widget", "Multi Chart")
@@ -48,9 +139,17 @@ export class ChartPanel extends Border2 implements IClickHandler {
     constructor() {
         super();
         this._tag = "div";
-        this._titleBar.buttons([this._buttonDownload, new Spacer(this), this._toggleLegend]);
+        this._titleBar.buttons(this.button_json().map(n => {
+            switch (n.type) {
+                case "Button":
+                    n.ref = new Button(this, n.icon);
+                    return n.ref;
+                case "ToggleButton":
+                    n.ref = new ToggleButton(this, n.icon).selected(!!n.selected);
+                    return n.ref;
+            }
+        }));
     }
-
     columns(): string[];
     columns(_: string[], asDefault?: boolean): this;
     columns(_?: string[], asDefault?: boolean): string[] | this {
@@ -77,6 +176,8 @@ export class ChartPanel extends Border2 implements IClickHandler {
         this.center(this._widget);
         this.right(this._legend);
 
+        this._modal.target(this._target).relativeTargetId(this.id());
+
         this._legend
             .targetWidget(this._widget)
             .orientation("vertical")
@@ -91,15 +192,11 @@ export class ChartPanel extends Border2 implements IClickHandler {
             .columns(this._legend.filteredColumns())
             .data(this._legend.filteredData())
             ;
-        if (this._prevChartDataFamily !== "ND") { // this._widget.getChartDataFamily()) {
-            this._prevChartDataFamily = "ND"; // this._widget.getChartDataFamily();
-            switch (this._prevChartDataFamily) {
-                case "any":
-                    this._toggleLegend.selected(false);
-                    this._legend.visible(false);
-                    break;
+        this.button_json().forEach(n => {
+            if (n.update) {
+                n.update.call(this, n);
             }
-        }
+        });
         this._legend.dataFamily("ND"); // this._widget.getChartDataFamily());
         super.update(domNode, element);
     }
@@ -110,19 +207,46 @@ export class ChartPanel extends Border2 implements IClickHandler {
 
     // IClickHandler  ---
     titleBarClick(src: Item, d, idx: number, groups): void {
-        switch (src) {
-            case this._buttonDownload:
-                this.downloadCSV();
-                break;
-            case this._toggleLegend:
-                if (this._toggleLegend.selected()) {
-                    this._legend.visible(true);
-                } else {
-                    this._legend.visible(false);
+        const clicked_button_arr = this.button_json().filter(n => n.ref === src);
+        const args = arguments;
+        clicked_button_arr.forEach(n => {
+            if (src instanceof Button && n.click) {
+                n.click.apply(this, args);
+            } else if (src instanceof ToggleButton) {
+                if (n.click) {
+                    n.click.apply(this, args);
                 }
-                this.render();
-                break;
-        }
+                if (src.selected()) {
+                    if (n.on) {
+                        if (n.init) n.init.apply(this, args);
+                        n.on.apply(this, args);
+                    }
+                } else {
+                    if (n.off) {
+                        n.off.apply(this, args);
+                    }
+                }
+            }
+        });
+    }
+    titleBarMouseEnter(src: Item, d, idx: number, groups): void {
+        const clicked_button_arr = this.button_json().filter(n => n.ref === src);
+        const args = arguments;
+        clicked_button_arr.forEach(n => {
+            if (n.mouseenter && n.init) {
+                if (n.init) n.init.apply(this, args);
+                n.mouseenter.apply(this, args);
+            }
+        });
+    }
+    titleBarMouseLeave(src: Item, d, idx: number, groups): void {
+        const clicked_button_arr = this.button_json().filter(n => n.ref === src);
+        const args = arguments;
+        clicked_button_arr.forEach(n => {
+            if (n.mouseleave) {
+                n.mouseleave.apply(this, args);
+            }
+        });
     }
 
     //  Event Handlers  ---

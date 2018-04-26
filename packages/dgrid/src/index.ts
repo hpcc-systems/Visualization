e1544c4160e61a7932f147e3099ff7ce5dfcecf1
Gordon Smith <gordonjsmith@gmail.com>
2018-04-26 08:54:41 +0100

    GH-2496 Add missing dependencies
    Fixes GH-2496
    
    Signed-off-by: Gordon Smith <gordonjsmith@gmail.com>

--- packages/dgrid/src/index.ts
+++ packages/dgrid/src/index.ts
@@ -1,6 +1,8 @@
-export * from "@hpcc-js/dgrid-shim";
 export * from "./Common";
 export * from "./DatasourceStore";
 export * from "./DatasourceTable";
 export * from "./RowFormatter";
 export * from "./Table";
+
+//  Must be last...
+export * from "@hpcc-js/dgrid-shim";

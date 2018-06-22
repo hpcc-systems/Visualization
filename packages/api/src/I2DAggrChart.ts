c5591c38eabf13d33267f7f559b70d95bb5b7b98
Gordon Smith <gordonjsmith@gmail.com>
2018-06-22 08:11:30 +0100

    feat(HexBin): Add selection support
    Signed-off-by: Gordon Smith <gordonjsmith@gmail.com>

---
+++ packages/api/src/I2DAggrChart.ts
@@ -0,0 +1,27 @@
+import { Palette } from "@hpcc-js/common";
+import { hsl as d3Hsl } from "d3-color";
+
+export function I2DAggrChart() {
+}
+I2DAggrChart.prototype._palette = Palette.rainbow("default");
+
+I2DAggrChart.prototype.fillColor = function (row: any[][], column, value): string {
+    return this._palette(row.length);
+};
+
+I2DAggrChart.prototype.strokeColor = function (row: any[][], column, value): string {
+    return d3Hsl(this.fillColor(row, column, value)).darker().toString();
+};
+
+I2DAggrChart.prototype.textColor = function (row: any[][], column, value): string {
+    return Palette.textColor(this.fillColor(row, column, value));
+};
+
+//  Events  ---
+I2DAggrChart.prototype.click = function (row: object[], column, selected) {
+    console.log("Click:  " + JSON.stringify(row) + ", " + column + ", " + selected);
+};
+
+I2DAggrChart.prototype.dblclick = function (row: object[], column, selected) {
+    console.log("Double click:  " + JSON.stringify(row) + ", " + column + ", " + selected);
+};

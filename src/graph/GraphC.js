107b00b2ae36be43fe3297f8b75cb57cec1b4caf
Jaman Brundage <jbrundage372@gmail.com>
2018-06-26 13:18:40 -0400

    fix(graphc): Add edge color support via published properties Fixes GH-2619
    Signed-off-by: Jaman Brundage <jbrundage372@gmail.com>

--- src/graph/GraphC.js
+++ src/graph/GraphC.js
@@ -198,6 +198,18 @@
 
     };
 
+    GraphC.prototype.linkcolor_default = function (_) {
+        if (!arguments.length) return this._linkcolor;
+        this._linkcolor = _;
+        return this;
+    }
+
+    GraphC.prototype.linktooltip_default = function (_) {
+        if (!arguments.length) return this._linktooltip;
+        this._linktooltip = _;
+        return this;
+    }
+
     GraphC.prototype.enter = function (domNode, element) {
         CanvasWidget.prototype.enter.apply(this, arguments);
         var context = this;

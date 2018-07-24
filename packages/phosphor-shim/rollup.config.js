6e42c5a655353a823c349d10efcc63cd270acbcb
Gordon Smith <gordonjsmith@gmail.com>
2018-07-24 08:45:30 +0100

    fix(phosphor): Remove "crypto" dependency
    Signed-off-by: Gordon Smith <gordonjsmith@gmail.com>

--- packages/phosphor-shim/rollup.config.js
+++ packages/phosphor-shim/rollup.config.js
@@ -2,6 +2,7 @@ import alias from 'rollup-plugin-alias';
 import commonjs from 'rollup-plugin-commonjs';
 import nodeResolve from 'rollup-plugin-node-resolve';
 import postcss from "rollup-plugin-postcss";
+import replace from 'rollup-plugin-replace';
 
 const pkg = require("./package.json");
 const name = pkg.name.split("/").pop();
@@ -34,6 +35,10 @@ export default {
         name: pkg.name
     }],
     plugins: [
+        replace({
+            "(typeof require !== 'undefined' && require('crypto')) ||": "/*---@hpcc-js (typeof require !== 'undefined' && require('crypto')) || @hpcc-js---*/",
+            delimiters: ['', '']
+        }),
         nodeResolve({
             preferBuiltins: true
         }),
@@ -41,8 +46,7 @@ export default {
             namedExports: {
                 "@phosphor/algorithm": ["each"],
                 "@phosphor/widgets": ["BoxPanel", "CommandRegistry", "CommandPalette", "ContextMenu", "DockLayout", "DockPanel", "Message", "Menu", "MenuBar", "SplitPanel", "TabBar", "TabPanel", "Widget"]
-            },
-            ignore: ['crypto']
+            }
         }),
         alias({
         }),

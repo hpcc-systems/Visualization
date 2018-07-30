2e94598d4a2c7e093c3857cfbb3bc98382d98d25
Gordon Smith <gordonjsmith@gmail.com>
2018-07-30 13:26:13 +0100

    chore(bundle):  Add missing file
    Signed-off-by: Gordon Smith <gordonjsmith@gmail.com>

---
+++ utils/bundle/src/index.ts
@@ -0,0 +1,15 @@
+const node_libs = ["child_process", "fs", "node-fetch", "os", "path", "semver", "safe-buffer", "tmp", "xmldom"];
+export function external(id) {
+    return id.indexOf("d3-") === 0 ||
+        id.indexOf("@hpcc-js/dgrid-shim") === 0 ||
+        id.indexOf("@hpcc-js/ddl-shim") === 0 ||
+        (id.indexOf("@hpcc-js") === 0 && id.indexOf("-shim") < 0) ||
+        node_libs.indexOf(id) >= 0;
+}
+
+export function globals(id) {
+    if (id.indexOf("@hpcc-js") === 0) {
+        return id;
+    }
+    return undefined;
+}

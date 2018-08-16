9513146cc756dd28a1206f1b9cfd4e0edef7df90
Jaman Brundage <jbrundage372@gmail.com>
2018-08-16 15:18:23 -0400

    feat(gallery): Add 6 tree samples
    Signed-off-by: Jaman Brundage <jbrundage372@gmail.com>

---
+++ demos/gallery/samples/tree/Sunburst/Sunburst.js
@@ -0,0 +1,40 @@
+import { SunburstPartition } from '@hpcc-js/tree';
+
+new SunburstPartition()
+    .data(getData())
+    .render()
+    ;
+
+function getData() {
+    return {
+        label: "root",
+        children: [{
+            label: "A",
+            children: [{
+                label: "AA",
+                children: [{
+                    label: "AAA",
+                    size: 144
+                }]
+            }, {
+                label: "AB",
+                children: [{
+                    label: "ABA",
+                    size: 89
+                }]
+            }]
+        }, {
+            label: "B",
+            children: [{
+                label: "BA",
+                children: [{
+                    label: "BAA",
+                    size: 54
+                }]
+            }, {
+                label: "BB",
+                size: 89
+            }]
+        }]
+    };
+}

a414ca9458a819d19a4b60b7b59da1d1aca0f9fe
Jaman Brundage <jbrundage372@gmail.com>
2018-08-16 13:21:22 -0400

    feat(gallery): Add BubbleXY sample
    Signed-off-by: Jaman Brundage <jbrundage372@gmail.com>

---
+++ demos/gallery/samples/chart/BubbleXY/BubbleXY.js
@@ -0,0 +1,20 @@
+import { BubbleXY } from "@hpcc-js/chart";
+
+new BubbleXY()
+    .target("target")
+    .columns(["Category", "Value 1", "Value 2"])
+    .data([
+        ["A", 34, 21],
+        ["B", 55, 34],
+        ["C", 54, 90],
+        ["D", 80, 153],
+        ["E", 86, 92],
+        ["F", 144, 233]
+    ])
+    .pointSizeColumn("Value 2")
+    .pointShape("circle")
+    .minPointSize(10)
+    .maxPointSize(30)
+    .tooltipValueFormat(",.0f")
+    .render()
+    ;

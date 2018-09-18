0e268fff1d629b2ace1b93c04edfad96affd1afc
Gordon Smith <gordonjsmith@gmail.com>
2018-09-18 16:17:24 +0100

    fix(ddl): Remove "0" annotations during the upgrade process
    Signed-off-by: Gordon Smith <gordonjsmith@gmail.com>

--- packages/ddl-shim/src/upgrade.ts
+++ packages/ddl-shim/src/upgrade.ts
@@ -333,10 +333,13 @@ class DDLUpgrade {
                         newValue[key] = valueMappings[value][key];
                     }
                 }
-                retVal.push({
-                    value,
-                    newValue
-                });
+                //  remove v1.x "0" annotations as they equated to "nothing"  ---
+                if (!annotation || value !== "0") {
+                    retVal.push({
+                        value,
+                        newValue
+                    });
+                }
             }
         }
         return retVal;

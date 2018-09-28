6c95c578379ab03601dcc81c444a987f0600d00e
Jaman Brundage <jbrundage372@gmail.com>
2018-09-28 16:03:47 -0400

    feat: Add randomized palette sample Fixes #2900 Signed-off-by: Jaman Brundage <jbrundage372@gmail.com>

---
+++ demos/gallery/samples/palette/Ordinal/RandomSamples.js
@@ -0,0 +1,24 @@
+import { Column } from "@hpcc-js/chart";
+import { Palette } from "@hpcc-js/common";
+
+var all_palettes = Column.prototype._palette.switch();
+
+var widget = new Column()
+    .target("target")
+    .paletteID(all_palettes[Math.floor(Math.random()*all_palettes.length)])
+    .columns(["Subject", "Year 1", "Year 2", "Year 3"])
+    .data([
+        ["Geography", 75, 68, 65],
+        ["English", 45, 55, -52],
+        ["Math", 98, 92, 90],
+        ["Science", 66, 60, 72]
+    ])
+    .render()
+    ;
+
+setInterval(function(){
+    widget
+        .paletteID(all_palettes[Math.floor(Math.random()*all_palettes.length)])
+        .render()
+        ;
+}, 1000)

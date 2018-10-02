ccc0c6cc3e79acd860fbb388c577a4f1d8248be3
Jaman Brundage <jbrundage372@gmail.com>
2018-10-02 11:05:46 -0400

    feat: Add option to show legend values Fixes #2887 Fixes #2892 Signed-off-by: Jaman Brundage <jbrundage372@gmail.com>

---
+++ demos/gallery/samples/legend/Values.js
@@ -0,0 +1,19 @@
+import { Column } from "@hpcc-js/chart";
+import { ChartPanel } from "@hpcc-js/layout";
+
+new ChartPanel()
+    .target("target")
+    .widget(new Column())
+    .columns(["Subject", "Year 1", "Year 2", "Year 3"])
+    .data([
+        ["Geography", 75, 68, 65],
+        ["English", 45, 55, -52],
+        ["Math", 98, 92, 90],
+        ["Science", 66, 60, 72]
+    ])
+    .title("Exam Results 2008->10")
+    .legendVisible(true)
+    .legend_showSeriesTotal(true)
+    .legend_showLegendTotal(true)
+    .render()
+    ;

7581c262adf41da704f48be871990ba441830973
Gordon Smith <gordonjsmith@gmail.com>
2018-10-22 11:20:23 +0100

    chore(gallery): Add option to "test" single sample easily
    Signed-off-by: Gordon Smith <gordonjsmith@gmail.com>

--- demos/gallery/src/test.ts
+++ demos/gallery/src/test.ts
@@ -4,12 +4,18 @@ declare const System: any;
 declare const describe: any;
 declare const it: any;
 
-describe("gallery", () => {
-    loadAllPaths();
-});
+const testPath = decodeURIComponent(window.location.search.split("?")[1]);
+if (testPath) {
+    loadPath(testPath);
+} else {
+    describe("gallery", () => {
+        loadAllPaths();
+    });
+}
 
 async function loadAllPaths() {
     for (const file of sampleFiles) {
+        console.log(file.path);
         if (file.type === "file") {
             it(file.path, function () {
                 return loadPath(file.path);

4e0f545820a021b63d82c7487611ad5b8a3ad851
Gordon Smith <gordonjsmith@gmail.com>
2019-04-01 07:28:18 +0100

    chore(test): Update local test server to localhost
    Signed-off-by: Gordon Smith <gordonjsmith@gmail.com>

--- tests/test-comms/src/testLib.ts
+++ tests/test-comms/src/testLib.ts
@@ -1,8 +1,8 @@
 import { isBrowser, isTravis as utilIsTravis } from "@hpcc-js/util";
 
 export const isTravis = utilIsTravis;
-export const ESP_URL = isTravis ? "http://52.210.14.156:8010/" : "http://192.168.3.22:8010/";
-export const QUERY_URL = isTravis ? "http://52.210.14.156:8002/" : "http://192.168.3.22:8002/";
+export const ESP_URL = isTravis ? "http://52.210.14.156:8010/" : "http://localhost:8010/";
+export const QUERY_URL = isTravis ? "http://52.210.14.156:8002/" : "http://localhost:8002/";
 
 console.log(`Test Server:  ${ESP_URL}`);
 console.log(`isTravis:  ${isTravis}`);

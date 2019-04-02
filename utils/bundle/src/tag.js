af9585c82989ebcdf6deeef454369fd119908adc
Gordon Smith <gordonjsmith@gmail.com>
2019-04-02 11:06:23 +0100

    chore(build): GitHub tagging
    Signed-off-by: Gordon Smith <gordonjsmith@gmail.com>

--- utils/bundle/src/tag.js
+++ utils/bundle/src/tag.js
@@ -1,8 +1,34 @@
 const fs = require("fs");
-var exec = require('child_process').exec;
+var execSync = require('child_process').execSync;
+
+function tagPush(pkgName, version) {
+    const verStr = `${pkgName}@${version}`;
+    return new Promise((resolve, reject) => {
+        try {
+            execSync(`git tag ${verStr}`);
+            execSync(`git push upstream ${verStr}`);
+            console.log(`tagged  ${verStr}`);
+        } catch (e) {
+            console.log(`skipped ${verStr}`);
+        }
+        resolve();
+    });
+}
 
 const wd = process.cwd();
 const loaderPkg = require(`${wd}/packages/loader/package.json`);
-exec(`git tag v${loaderPkg.version}`, function (error, stderr, stdout) {
-    exec(`git push upstream v${loaderPkg.version}`, function (error, stderr, stdout) { });
-});
+const promises = [];
+promises.push(tagPush(loaderPkg.name, loaderPkg.version));
+for (const key in loaderPkg.dependencies) {
+    if (key.indexOf("@hpcc-js") === 0) {
+        let version = loaderPkg.dependencies[key];
+        if (version[0] === "^" || version[0] === "~") {
+            version = version.substring(1);
+        }
+        promises.push(tagPush(key, version));
+    }
+}
+
+Promise.all(promises).then(() => {
+    console.log("...complete...");
+})

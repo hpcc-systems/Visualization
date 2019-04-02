36dee268accd5686e69821e1c8d46a28bf2662ec
Gordon Smith <gordonjsmith@gmail.com>
2019-04-02 13:35:40 +0100

    chore(build): GitHub Tag / Version Testing
    Signed-off-by: Gordon Smith <gordonjsmith@gmail.com>

--- utils/bundle/src/tag.js
+++ utils/bundle/src/tag.js
@@ -1,34 +1,13 @@
 const fs = require("fs");
 var execSync = require('child_process').execSync;
 
-function tagPush(pkgName, version) {
-    const verStr = `${pkgName}@${version}`;
-    return new Promise((resolve, reject) => {
-        try {
-            execSync(`git tag -a -m "chore(tag): v${version}" ${verStr}`);
-            execSync(`git push upstream ${verStr}`);
-            console.log(`tagged  ${verStr}`);
-        } catch (e) {
-            console.log(`skipped ${verStr}`);
-        }
-        resolve();
-    });
-}
-
 const wd = process.cwd();
 const loaderPkg = require(`${wd}/packages/loader/package.json`);
-const promises = [];
-promises.push(tagPush(loaderPkg.name, loaderPkg.version));
-for (const key in loaderPkg.dependencies) {
-    if (key.indexOf("@hpcc-js") === 0) {
-        let version = loaderPkg.dependencies[key];
-        if (version[0] === "^" || version[0] === "~") {
-            version = version.substring(1);
-        }
-        promises.push(tagPush(key, version));
-    }
-}
 
-Promise.all(promises).then(() => {
-    console.log("...complete...");
-})
+try {
+    execSync(`git tag -a -m "chore(tag): v${loaderPkg.version}" v${loaderPkg.version}`);
+    execSync(`git push upstream v${loaderPkg.version}`);
+    console.log(`tagged  v${loaderPkg.version}`);
+} catch (e) {
+    console.log(`skipped v${loaderPkg.version}`);
+}

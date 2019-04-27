78b5081b30f38d1427cbb458b2af9a04b8ccff6c
Gordon Smith <gordonjsmith@gmail.com>
2019-04-27 03:24:19 +0100

    chore(publish): Bump versions and build all

--- packages/eclwatch/src/__package__.ts
+++ packages/eclwatch/src/__package__.ts
@@ -1,3 +1,3 @@
 export const PKG_NAME = "@hpcc-js/eclwatch";
-export const PKG_VERSION = "2.5.5";
-export const BUILD_VERSION = "2.6.5";
+export const PKG_VERSION = "2.5.7";
+export const BUILD_VERSION = "2.6.7";
--- packages/layout/src/__package__.ts
+++ packages/layout/src/__package__.ts
@@ -1,3 +1,3 @@
 export const PKG_NAME = "@hpcc-js/layout";
-export const PKG_VERSION = "2.12.5";
-export const BUILD_VERSION = "2.6.5";
+export const PKG_VERSION = "2.12.7";
+export const BUILD_VERSION = "2.6.7";

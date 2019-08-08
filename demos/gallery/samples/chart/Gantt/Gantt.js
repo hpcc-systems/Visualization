df510f78ee855a36bcabc87caebd945adba35116
Gordon Smith <gordonjsmith@gmail.com>
2019-08-08 16:14:30 +0100

    fix(demo): Gantt demo missing yAxisType of "time"
    Fixes #3305
    
    Signed-off-by: Gordon Smith <gordonjsmith@gmail.com>

--- demos/gallery/samples/chart/Gantt/Gantt.js
+++ demos/gallery/samples/chart/Gantt/Gantt.js
@@ -8,6 +8,7 @@ new Gantt()
         ["Coding", ["2011-08-09", "2012-09-09"]],
         ["Specs", ["2010-07-09", "2011-08-09"]]
     ])
+    .yAxisType("time")
     .yAxisTypeTimePattern("%Y-%m-%d")
     .render()
     ;

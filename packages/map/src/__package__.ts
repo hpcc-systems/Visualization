98dbffa00707520be15e8d0805220f31357c8458
Gordon Smith <gordonjsmith@gmail.com>
2019-10-08 17:26:54 +0100

    chore(publish): Bump versions and build all

--- packages/map/src/__package__.ts
+++ packages/map/src/__package__.ts
@@ -1,3 +1,3 @@
 export const PKG_NAME = "@hpcc-js/map";
-export const PKG_VERSION = "2.13.0";
-export const BUILD_VERSION = "2.10.5";
+export const PKG_VERSION = "2.14.0";
+export const BUILD_VERSION = "2.10.6";

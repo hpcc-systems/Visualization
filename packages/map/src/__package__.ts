5c7f712ebbc5864d06b9d85be5964de165893f00
Gordon Smith <gordonjsmith@gmail.com>
2020-01-29 09:07:13 +0000

    chore(publish): Bump versions and build all

--- packages/map/src/__package__.ts
+++ packages/map/src/__package__.ts
@@ -1,3 +1,3 @@
 export const PKG_NAME = "@hpcc-js/map";
-export const PKG_VERSION = "2.14.5";
-export const BUILD_VERSION = "2.12.2";
+export const PKG_VERSION = "2.14.6";
+export const BUILD_VERSION = "2.12.3";
--- packages/timeline/src/__package__.ts
+++ packages/timeline/src/__package__.ts
@@ -1,3 +1,3 @@
 export const PKG_NAME = "@hpcc-js/timeline";
 export const PKG_VERSION = "2.7.10";
-export const BUILD_VERSION = "2.12.2";
+export const BUILD_VERSION = "2.12.3";

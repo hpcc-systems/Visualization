22f7e506ac910408655c7778f8a3a68bdfea5364
Gordon Smith <gordonjsmith@gmail.com>
2020-03-02 08:02:58 +0000

    chore(publish): Bump versions and build all

--- packages/api/src/__package__.ts
+++ packages/api/src/__package__.ts
@@ -1,3 +1,3 @@
 export const PKG_NAME = "@hpcc-js/api";
-export const PKG_VERSION = "2.8.8";
-export const BUILD_VERSION = "2.12.9";
+export const PKG_VERSION = "2.8.9";
+export const BUILD_VERSION = "2.13.0";
--- packages/dgrid-shim/src/__package__.ts
+++ packages/dgrid-shim/src/__package__.ts
@@ -1,3 +1,3 @@
 export const PKG_NAME = "@hpcc-js/dgrid-shim";
-export const PKG_VERSION = "2.11.5";
-export const BUILD_VERSION = "2.12.9";
+export const PKG_VERSION = "2.11.6";
+export const BUILD_VERSION = "2.13.0";

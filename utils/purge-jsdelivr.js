432e10df9f82e31cf92b449720e8e3f8d016fee9
Gordon Smith <gordonjsmith@gmail.com>
2020-03-26 11:34:56 +0000

    chore(build): Add script to purge JSDelivr CDN
    Signed-off-by: Gordon Smith <gordonjsmith@gmail.com>

---
+++ utils/purge-jsdelivr.js
@@ -0,0 +1,39 @@
+const https = require('https');
+
+function get(id, url) {
+    return new Promise((resolve, reject) => {
+        https.get(url, resp => {
+            let data = '';
+            resp.on('data', (chunk) => {
+                data += chunk;
+            });
+            resp.on('end', () => {
+                resolve(data);
+            });
+        }).on("error", (err) => {
+            reject(err);
+        });
+    });
+}
+
+function purgePackage(id, ver) {
+    const purgeIndex = `https://purge.jsdelivr.net/npm/${id}@${ver}/dist/index.js`;
+    const purgeIndexMin = `https://purge.jsdelivr.net/npm/${id}@${ver}/dist/index.min.js`;
+    const purgePackage = `https://purge.jsdelivr.net/npm/${id}@${ver}/package.json`;
+    return Promise.all([get(id, purgeIndex), get(id, purgeIndexMin), get(id, purgePackage)]).then(responses => {
+        return {
+            id,
+            responses
+        };
+    });
+}
+
+const wd = process.cwd();
+console.log(`${wd}/packages/loader/package.json`);
+const pkg = require(`${wd}/packages/loader/package.json`);
+for (const key in pkg.dependencies) {
+    purgePackage(key, pkg.dependencies[key].replace("^", ""))
+        .then(console.log)
+        .catch(e => console.error(e.message))
+        ;
+}

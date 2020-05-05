d48f76515e56f29a701341ed9dea78dbfac389b1
Gordon Smith <GordonJSmith@gmail.com>
2020-05-05 16:29:58 +0100

    fix(MapBox): Upgrade to latest MapBox API
    Previous API expires June 1, 2020
    
    Signed-off-by: Gordon Smith <GordonJSmith@gmail.com>

--- packages/map/src/leaflet/MapBox.ts
+++ packages/map/src/leaflet/MapBox.ts
@@ -24,10 +24,10 @@ export class MapBoxLayer extends TileLayer {
             console.warn("__hpcc_mapbox_apikey does not contain a valid API key, reverting to developers key (expect limited performance)");
         }
         const hpcc_mapbox_apikey = window.__hpcc_mapbox_apikey || "pk.eyJ1IjoibGVzY2htb28iLCJhIjoiY2psY2FqY3l3MDhqNDN3cDl1MzFmZnkwcCJ9.HRoFwmz1j80gyz18ruggqw";
-        this.add(new LeafletTileLayer(`https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=${hpcc_mapbox_apikey}`, {
+        this.add(new LeafletTileLayer(`https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token=${hpcc_mapbox_apikey}`, {
             attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
             maxZoom: 18,
-            id: "mapbox.streets",
+            id: "streets-v11",
             accessToken: hpcc_mapbox_apikey
         } as any));
     }

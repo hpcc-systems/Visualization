ac0145bb8b796b2b92ad13829dd4abd6360c2b0a
Gordon Smith <GordonJSmith@gmail.com>
2020-07-29 08:03:01 +0100

    feat(comms): Include functions when creating graph
    Signed-off-by: Gordon Smith <GordonJSmith@gmail.com>

--- packages/comms/src/ecl/workunit.ts
+++ packages/comms/src/ecl/workunit.ts
@@ -582,7 +582,7 @@ export class Workunit extends StateObject<UWorkunitState, IWorkunitState> implem
         });
     }
 
-    fetchGraphDetails(graphIDs: string[] = [], rootTypes: string[], nestedTypes = ["graph", "subgraph", "activity", "edge"]): Promise<BaseScope[]> {
+    fetchGraphDetails(graphIDs: string[] = [], rootTypes: string[]): Promise<BaseScope[]> {
         return this.fetchDetails({
             ScopeFilter: {
                 MaxDepth: 999999,
@@ -591,7 +591,7 @@ export class Workunit extends StateObject<UWorkunitState, IWorkunitState> implem
             },
             NestedFilter: {
                 Depth: 999999,
-                ScopeTypes: nestedTypes
+                ScopeTypes: ["graph", "subgraph", "activity", "edge", "function"]
             },
             PropertiesToReturn: {
                 AllStatistics: true,

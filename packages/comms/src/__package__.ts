05178000aa1030c4c1bc56f7c535dd7312f8c806
Gordon Smith <GordonJSmith@gmail.com>
2020-09-18 15:23:45 +0100

    chore(publish): Bump versions and build all

--- packages/comms/src/__package__.ts
+++ packages/comms/src/__package__.ts
@@ -1,3 +1,3 @@
 export const PKG_NAME = "@hpcc-js/comms";
-export const PKG_VERSION = "2.26.0";
-export const BUILD_VERSION = "2.40.0";
+export const PKG_VERSION = "2.27.0";
+export const BUILD_VERSION = "2.41.0";
--- packages/marshaller/src/__package__.ts
+++ packages/marshaller/src/__package__.ts
@@ -1,3 +1,3 @@
 export const PKG_NAME = "@hpcc-js/marshaller";
-export const PKG_VERSION = "2.23.50";
-export const BUILD_VERSION = "2.40.0";
+export const PKG_VERSION = "2.23.51";
+export const BUILD_VERSION = "2.41.0";

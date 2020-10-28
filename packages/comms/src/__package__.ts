bb249fc97a0add08cfa303c08cc2c2a0affb3c04
Gordon Smith <GordonJSmith@gmail.com>
2020-10-28 08:10:48 +0000

    chore(publish): Bump versions and build all

--- packages/comms/src/__package__.ts
+++ packages/comms/src/__package__.ts
@@ -1,3 +1,3 @@
 export const PKG_NAME = "@hpcc-js/comms";
-export const PKG_VERSION = "2.30.0";
-export const BUILD_VERSION = "2.46.0";
+export const PKG_VERSION = "2.31.0";
+export const BUILD_VERSION = "2.47.0";
--- packages/layout/src/__package__.ts
+++ packages/layout/src/__package__.ts
@@ -1,3 +1,3 @@
 export const PKG_NAME = "@hpcc-js/layout";
 export const PKG_VERSION = "2.19.0";
-export const BUILD_VERSION = "2.46.0";
+export const BUILD_VERSION = "2.47.0";

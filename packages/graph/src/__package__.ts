b7a09b09fef8b7370d7b52cc0f972116ef61d47f
Gordon Smith <GordonJSmith@gmail.com>
2021-02-01 06:25:19 +0000

    chore(publish): Bump versions and build all

--- packages/graph/src/__package__.ts
+++ packages/graph/src/__package__.ts
@@ -1,3 +1,3 @@
 export const PKG_NAME = "@hpcc-js/graph";
 export const PKG_VERSION = "2.47.0";
-export const BUILD_VERSION = "2.51.0";
+export const BUILD_VERSION = "2.52.0";
--- packages/observable-md/src/__package__.ts
+++ packages/observable-md/src/__package__.ts
@@ -1,3 +1,3 @@
 export const PKG_NAME = "@hpcc-js/observable-md";
-export const PKG_VERSION = "2.24.0";
-export const BUILD_VERSION = "2.51.0";
+export const PKG_VERSION = "2.25.0";
+export const BUILD_VERSION = "2.52.0";

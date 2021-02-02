e5f209ff2832478a3f0adddacab9ff506b2f491b
Gordon Smith <GordonJSmith@gmail.com>
2021-02-02 07:39:27 +0000

    chore(publish): Bump versions and build all

--- packages/html/src/__package__.ts
+++ packages/html/src/__package__.ts
@@ -1,3 +1,3 @@
 export const PKG_NAME = "@hpcc-js/html";
 export const PKG_VERSION = "2.20.0";
-export const BUILD_VERSION = "2.52.0";
+export const BUILD_VERSION = "2.53.0";
--- packages/marshaller/src/__package__.ts
+++ packages/marshaller/src/__package__.ts
@@ -1,3 +1,3 @@
 export const PKG_NAME = "@hpcc-js/marshaller";
-export const PKG_VERSION = "2.23.61";
-export const BUILD_VERSION = "2.52.0";
+export const PKG_VERSION = "2.23.62";
+export const BUILD_VERSION = "2.53.0";

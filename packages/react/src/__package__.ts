e8590d4f2435da6f190361f63cc94fd904f4b5bc
Gordon Smith <GordonJSmith@gmail.com>
2021-02-12 09:50:22 +0000

    chore(publish): Bump versions and build all

--- packages/react/src/__package__.ts
+++ packages/react/src/__package__.ts
@@ -1,3 +1,3 @@
 export const PKG_NAME = "@hpcc-js/react";
 export const PKG_VERSION = "2.27.0";
-export const BUILD_VERSION = "2.54.0";
+export const BUILD_VERSION = "2.55.0";
--- packages/util/src/__package__.ts
+++ packages/util/src/__package__.ts
@@ -1,3 +1,3 @@
 export const PKG_NAME = "@hpcc-js/util";
 export const PKG_VERSION = "2.28.0";
-export const BUILD_VERSION = "2.54.0";
+export const BUILD_VERSION = "2.55.0";

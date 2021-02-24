2e710eb69c271a9ce15bc7be6849530cb66a2a7b
Gordon Smith <GordonJSmith@gmail.com>
2021-02-24 11:29:25 +0000

    chore(publish): Bump versions and build all

--- packages/eclwatch/src/__package__.ts
+++ packages/eclwatch/src/__package__.ts
@@ -1,3 +1,3 @@
 export const PKG_NAME = "@hpcc-js/eclwatch";
-export const PKG_VERSION = "2.30.0";
-export const BUILD_VERSION = "2.57.0";
+export const PKG_VERSION = "2.31.0";
+export const BUILD_VERSION = "2.58.0";

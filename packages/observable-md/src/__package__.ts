8a3f388c180c318410bfa81575f58c134dd72822
Gordon Smith <GordonJSmith@gmail.com>
2021-08-16 15:20:20 +0100

    chore(publish): Bump versions and build all

--- packages/observable-md/src/__package__.ts
+++ packages/observable-md/src/__package__.ts
@@ -1,3 +1,3 @@
 export const PKG_NAME = "@hpcc-js/observable-md";
 export const PKG_VERSION = "2.39.0";
-export const BUILD_VERSION = "2.79.0";
+export const BUILD_VERSION = "2.80.0";
--- packages/timeline/src/__package__.ts
+++ packages/timeline/src/__package__.ts
@@ -1,3 +1,3 @@
 export const PKG_NAME = "@hpcc-js/timeline";
 export const PKG_VERSION = "2.36.0";
-export const BUILD_VERSION = "2.79.0";
+export const BUILD_VERSION = "2.80.0";

70882146c0a0f04c5f1f6533323a228decd62d49
Gordon Smith <GordonJSmith@gmail.com>
2022-03-11 06:47:23 +0000

    fix(observable):  Observable shim was missing sources inside the bundle
    Signed-off-by: Gordon Smith <GordonJSmith@gmail.com>

--- packages/observable-shim/rollup.config.js
+++ packages/observable-shim/rollup.config.js
@@ -1,10 +1,9 @@
-import { external, globals } from "@hpcc-js/bundle";
 import sourcemaps from "rollup-plugin-sourcemaps";
 import nodeResolve from "@rollup/plugin-node-resolve";
 import postcss from "rollup-plugin-postcss";
+import { globals } from "@hpcc-js/bundle";
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const pkg = require("./package.json");
+import pkg from "./package.json";
 
 const plugins = [
     nodeResolve(),
@@ -17,7 +16,6 @@ const plugins = [
 
 export default [{
     input: "lib-es6/index",
-    external,
     output: [{
         file: pkg.main,
         format: "es",

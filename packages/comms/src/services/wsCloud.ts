f98e5cfb203dcdb2bc58f6bd5a85ed7e749275e4
Gordon Smith <GordonJSmith@gmail.com>
2022-04-19 14:09:27 +0100

    feat(WsCloud):  At type information for POD response
    Signed-off-by: Gordon Smith <GordonJSmith@gmail.com>

--- packages/comms/src/services/wsCloud.ts
+++ packages/comms/src/services/wsCloud.ts
@@ -1,8 +1,22 @@
+import type { V1Pod } from "@kubernetes/client-node";
 import { CloudServiceBase, WsCloud } from "./wsdl/WsCloud/v1/WsCloud";
 
 export {
-    WsCloud
+    WsCloud,
+    V1Pod
 };
 
 export class CloudService extends CloudServiceBase {
+
+    getPODs(): Promise<V1Pod[]> {
+        return super.GetPODs({}).then((response) => {
+            try {
+                const obj = typeof response.Result === "string" ? JSON.parse(response.Result) : response.Result;
+                return obj?.items ?? [];
+
+            } catch (error) {
+                return [];
+            }
+        });
+    }
 }

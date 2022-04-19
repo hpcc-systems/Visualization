061ed2153c8dbd321846c34db919cbb3ba817da9
Jeremy Clements <jeremy.clements@lexisnexisrisk.com>
2022-04-19 14:09:43 -0400

    fix(comms): add LogAccessType enum to exports in wsLogaccess & include wsDali in exported comms services
    Signed-off-by: Jeremy Clements <jeremy.clements@lexisnexisrisk.com>

--- packages/comms/src/services/wsLogaccess.ts
+++ packages/comms/src/services/wsLogaccess.ts
@@ -1,7 +1,7 @@
-import { LogaccessServiceBase, WsLogaccess } from "./wsdl/ws_logaccess/v1/ws_logaccess";
+import { LogaccessServiceBase, WsLogaccess, LogAccessType } from "./wsdl/ws_logaccess/v1/ws_logaccess";
 
 export {
-    WsLogaccess
+    WsLogaccess, LogAccessType
 };
 
 export class LogaccessService extends LogaccessServiceBase {

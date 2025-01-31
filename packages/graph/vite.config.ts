280db7a82c01ce1a7c5ac8713e02eb4df9609bf3
Gordon Smith <GordonJSmith@gmail.com>
2025-01-31 15:48:01 +0000

    fix: Prevent React from being included in the bundle
    Signed-off-by: Gordon Smith <GordonJSmith@gmail.com>

--- packages/graph/vite.config.ts
+++ packages/graph/vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
             fileName: "index",
         },
         rollupOptions: {
-            external,
+            external: [...external, "react", "react-dom"],
             output: {
                 globals,
             },

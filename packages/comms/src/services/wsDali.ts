4ef5cce206dab2689398b098efd87fb283fe47a2
Jeremy Clements <79224539+jeclrsg@users.noreply.github.com>
2025-04-16 10:12:14 -0400

    feat(comms): add Export function to WsDali service
    Signed-off-by: Jeremy Clements <79224539+jeclrsg@users.noreply.github.com>

--- packages/comms/src/services/wsDali.ts
+++ packages/comms/src/services/wsDali.ts
@@ -4,5 +4,37 @@ export {
     WsDali
 };
 
+export interface ExportRequest {
+    Path: string;
+    Safe: boolean;
+}
+
+export interface ExportResponse {
+    filename: string;
+    content: Blob;
+}
+
 export class DaliService extends DaliServiceBase {
+
+    async Export(request: ExportRequest): Promise<ExportResponse> {
+        const exportUrl = "/WsDali/Export";
+
+        const reqParams = new URLSearchParams();
+        if (request.Path) {
+            reqParams.append("Path", request.Path);
+        }
+        if (request.Safe !== undefined) {
+            reqParams.append("Safe", String(request.Safe));
+        }
+
+        const response = await fetch(`${exportUrl}?${reqParams.toString()}`);
+        const filename = response.headers.get("Content-Disposition");
+        const content = await response.blob();
+
+        return {
+            filename: filename ? filename.split("filename=")[1] : "WsDali_Export",
+            content
+        };
+    }
+
 }

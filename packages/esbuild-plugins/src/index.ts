e384ff6d29537e8e499ebdf66fb5512fd8a4d563
Gordon Smith <GordonJSmith@gmail.com>
2025-07-04 07:27:10 +0100

    fix: Bundle wasm deps into sfx-wrapper
    Signed-off-by: Gordon Smith <GordonJSmith@gmail.com>

--- packages/esbuild-plugins/src/index.ts
+++ packages/esbuild-plugins/src/index.ts
@@ -3,6 +3,5 @@ export * from "./exclude-sourcemap.ts";
 export * from "./problem-matcher.ts";
 export * from "./rebuild-logger.ts";
 export * from "./remove-strict.ts";
-export * from "./sfx-wrapper.ts";
 export * from "./inline-css.ts";
 export * from "./vite-utils.ts";

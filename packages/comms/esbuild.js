2dddfc42eb889ce7647cbfd3cb514c9327a23872
Gordon Smith <GordonJSmith@gmail.com>
2025-10-09 09:08:58 +0100

    fix: NodeJS bundle invalid
    Signed-off-by: Gordon Smith <GordonJSmith@gmail.com>

--- packages/comms/esbuild.js
+++ packages/comms/esbuild.js
@@ -2,6 +2,12 @@ import { nodeTpl, nodeBoth } from "@hpcc-js/esbuild-plugins";
 
 //  config  ---
 await Promise.all([
-    nodeBoth("src/index.node.ts", "dist/node/index", { packages: "bundle" }),
-    nodeTpl("utils/index.ts", "lib-esm/index")
+    nodeBoth("src/index.node.ts", "dist/node/index", {
+        packages: "external",
+        external: ["node:*"]
+    }),
+    nodeTpl("utils/index.ts", "lib-esm/index", {
+        packages: "external",
+        external: ["node:*"]
+    })
 ]);
--- packages/observablehq-compiler/esbuild.js
+++ packages/observablehq-compiler/esbuild.js
@@ -2,5 +2,8 @@ import { nodeBoth } from "@hpcc-js/esbuild-plugins";
 
 //  config  ---
 await Promise.all([
-    nodeBoth("src/index.node.ts", "dist/node/index")
+    nodeBoth("src/index.node.ts", "dist/node/index", {
+        packages: "external",
+        external: ["node:*"]
+    })
 ]);

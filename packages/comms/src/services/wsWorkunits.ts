47722ba1bef68a38d4922fba1b9825f33a0e059a
Jeremy Clements <79224539+jeclrsg@users.noreply.github.com>
2026-01-14 10:55:26 -0500

    feat: bump WsWorkunits and WsSMC (#4506)
    Signed-off-by: Jeremy Clements <79224539+jeclrsg@users.noreply.github.com>

--- packages/comms/src/services/wsWorkunits.ts
+++ packages/comms/src/services/wsWorkunits.ts
@@ -1,5 +1,5 @@
 import { deepMixin, xml2json, XMLNode } from "@hpcc-js/util";
-import { WsWorkunits, WorkunitsServiceBase } from "./wsdl/WsWorkunits/v2.02/WsWorkunits.ts";
+import { WsWorkunits, WorkunitsServiceBase } from "./wsdl/WsWorkunits/v2.03/WsWorkunits.ts";
 import { IConnection, IOptions } from "../connection.ts";
 
 export {

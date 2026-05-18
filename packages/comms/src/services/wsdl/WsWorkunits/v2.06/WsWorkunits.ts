b3fcd496c084a2d19cc21859f1a7e92443738399
Jeremy Clements <79224539+jeclrsg@users.noreply.github.com>
2026-05-18 02:53:11 -0400

    fix(comms): fix missing properties from recent WsWorkunit services (#4592)
    ensures that newer versions contain all changes from predecessors, ie
    v2.08 contains v2.07, and so on
    
    Signed-off-by: Jeremy Clements <79224539+jeclrsg@users.noreply.github.com>

--- packages/comms/src/services/wsdl/WsWorkunits/v2.06/WsWorkunits.ts
+++ packages/comms/src/services/wsdl/WsWorkunits/v2.06/WsWorkunits.ts
@@ -852,7 +852,6 @@ export namespace WsWorkunits {
         CompileCost?: double;
         NoAccess?: boolean;
         ECLWUProcessList?: ECLWUProcessList;
-        FailureDesc?: string;
     }
 
     export interface WUCreateResponse {
@@ -1987,7 +1986,6 @@ export namespace WsWorkunits {
         MinimumExecuteCost?: double;
         MinimumFileAccessCost?: double;
         MinimumCompileCost?: double;
-        ErrorContents?: string;
         Sortby?: string;
         Descending?: boolean;
         CacheHint?: long;
@@ -2072,7 +2070,6 @@ export namespace WsWorkunits {
         CompileCost?: double;
         NoAccess?: boolean;
         ECLWUProcessList?: ECLWUProcessList;
-        FailureDesc?: string;
     }
 
     export interface Workunits2 {
@@ -2906,7 +2903,7 @@ export namespace WsWorkunits {
 export class WorkunitsServiceBase extends Service {
 
     constructor(optsConnection: IOptions | IConnection) {
-        super(optsConnection, "WsWorkunits", "2.08");
+        super(optsConnection, "WsWorkunits", "2.06");
     }
 
     GVCAjaxGraph(request: WsWorkunits.GVCAjaxGraph, abortSignal?: AbortSignal): Promise<WsWorkunits.GVCAjaxGraphResponse> {

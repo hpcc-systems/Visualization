bbb409f68b3323673d358bef33bcf3ead35c87dd
Gordon Smith <GordonJSmith@gmail.com>
2026-05-31 11:29:07 +0100

    feat: enhance panel components with new sizing options and layout features
    Signed-off-by: Gordon Smith <GordonJSmith@gmail.com>

--- packages/phosphor/src/SplitPanel.ts
+++ packages/phosphor/src/SplitPanel.ts
@@ -1,36 +1,62 @@
 import { HTMLWidget, SVGWidget, Widget } from "@hpcc-js/common";
-import { Message, SplitPanel as PSplitPanel, Widget as PWidget } from "./phosphor-shim.ts";
-import { Msg, WidgetAdapter, WidgetAdapterArray } from "./WidgetAdapter.ts";
+import { BasePanel } from "./BasePanel.ts";
+import { SplitPanel as PSplitPanel, Widget as PWidget } from "./phosphor-shim.ts";
+import { WidgetAdapter, WidgetAdapterArray } from "./WidgetAdapter.ts";
 
 import "../src/DockPanel.css";
 
-export class SplitPanel extends HTMLWidget {
-    private _split: PSplitPanel;
-    private content = WidgetAdapterArray.create();
 
-    constructor(orientation: "horizontal" | "vertical" = "vertical") {
+export namespace SplitPanel {
+
+    export interface IAddWidgetOptions extends BasePanel.IAddWidgetOptions {
+        /** Maximum size in pixels */
+        maxSize?: number;
+    }
+}
+
+export class SplitPanel extends BasePanel {
+    private _split: PSplitPanel;
+    protected _content = WidgetAdapterArray.create();
+
+    constructor(readonly _orientation: "horizontal" | "vertical" = "vertical") {
         super();
-        this._split = new PSplitPanel({ orientation });
-        this._tag = "div";
+        this._split = new PSplitPanel({ orientation: _orientation });
         this._split.id = "p" + this.id();
     }
 
-    protected getWidgetAdapter(widget: Widget): WidgetAdapter | null {
-        let retVal = null;
-        this.content.some(wa => {
-            if (wa.widget === widget) {
-                retVal = wa;
-                return true;
-            }
-            return false;
-        });
-        return retVal;
-    }
-
-    addWidget(widget: SVGWidget | HTMLWidget) {
+    private _pendingDefaults: Map<number, { defaultSize?: number }> = new Map();
+    addWidget(widget: SVGWidget | HTMLWidget, options?: SplitPanel.IAddWidgetOptions): this {
+        const opts = options || {};
         const wa = new WidgetAdapter(this, widget);
+        wa.padding = opts.padding ?? 0;
+
+        if (opts.minSize != null || opts.maxSize != null) {
+            const style = wa.node.style;
+            if (opts.minSize != null) {
+                if (this._orientation === "horizontal") {
+                    style.minWidth = `${opts.minSize}px`;
+                } else {
+                    style.minHeight = `${opts.minSize}px`;
+                }
+            }
+            if (opts.maxSize != null) {
+                if (this._orientation === "horizontal") {
+                    style.maxWidth = `${opts.maxSize}px`;
+                } else {
+                    style.maxHeight = `${opts.maxSize}px`;
+                }
+            }
+        }
+
         this._split.addWidget(wa);
-        this.content.push(wa);
+        this._content.push(wa);
+
+        if (opts.defaultSize != null) {
+            this._pendingDefaults.set(this._content.length - 1, {
+                defaultSize: opts.defaultSize
+            });
+        }
+
         return this;
     }
 
@@ -62,26 +88,51 @@ export class SplitPanel extends HTMLWidget {
 
     render(callback?: (w: Widget) => void): this {
         return super.render(w => {
-            this.content.watchRendered(this, callback);
+            this._applyPendingDefaults();
+            this._content.watchRendered(this, callback);
             this._split.update();
         });
     }
 
-    private _prevActive: Widget;
-    processMessage(msg: Message): void {
-        switch (msg.type) {
-            case "wa-activate-request":
-                const widget = (msg as Msg.WAActivateRequest).wa.widget;
-                if (this._prevActive !== widget) {
-                    this._prevActive = widget;
-                    this.childActivation(widget);
+    private _applyPendingDefaults() {
+        if (this._pendingDefaults.size === 0) return;
+
+        const isHorizontal = this._orientation === "horizontal";
+        const totalSpace = isHorizontal ? this.width() : this.height();
+        const n = this._content.length;
+
+        let usedSpace = 0;
+        let flexCount = 0;
+        const pixelSizes: (number | null)[] = [];
+
+        for (let i = 0; i < n; i++) {
+            const defaults = this._pendingDefaults.get(i);
+            if (defaults) {
+                const size = defaults.defaultSize;
+                if (size != null) {
+                    pixelSizes.push(size);
+                    usedSpace += size;
+                    continue;
                 }
-                break;
+            }
+            pixelSizes.push(null);
+            flexCount++;
         }
+
+        this._pendingDefaults.clear();
+
+        const remainingSpace = Math.max(0, totalSpace - usedSpace);
+        const flexSize = flexCount > 0 ? remainingSpace / flexCount : 0;
+        const sizes = pixelSizes.map(px => px != null ? px : flexSize);
+        this._split.setRelativeSizes(sizes);
     }
 
-    // Events ---
-    childActivation(w: Widget) {
+    //  Events  ---
+    childActivation(w: Widget, wa: WidgetAdapter) {
+    }
+
+    layoutChanged() {
     }
 }
 SplitPanel.prototype._class += " phosphor_SplitPanel";
+
